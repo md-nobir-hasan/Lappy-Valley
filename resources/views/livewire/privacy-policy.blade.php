67ef319b103d99859b3097e53bbceb5d42bf7744
Sumon-Mia <sumon.wdd@gmail.com>
2023-11-01 10:18:53 +0600

    done privacy-policy page

--- resources/views/livewire/privacy-policy.blade.php
+++ resources/views/livewire/privacy-policy.blade.php
@@ -1,3 +1,103 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class='container mx-auto'>
+<div class=>
+            <h1 class='font-[jost] text-[16px] font-[400] tracking-[.48px] text-[#353535]'>Home / Refund & Service Policy 
+        </div>
+        <div class='h-[2px] bg-[#764A8733]'></div>
+
+     <div class='text-center mt-[40px] mb-[50px]'>
+        <h1 class='text-[#380D37] text-[36px] font-[jost] font-[700]'>Privacy Policy</h1>
+    </div>
+
+    <div class='text-left w-[1030px] h-[1796px] mx-auto'>
+       <div class='my-[25px]'>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px]'> Who we are</h1>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> Our website address is: https://lappyvalley.com.</p>
+       </div>
+
+       <div>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px]'> What personal data we collect and why we collect it:</h1>
+       </div>
+
+       <div class='my-[25px]'>
+        <span class='text-[16px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'> Comments</span>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor’s
+             IP address and browser user agent string to help spam detection.</p>
+
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it.
+             The Gravatar service privacy policy is available here: https://automattic.com/privacy/.
+             After approval of your comment, your profile picture is visible to the public in the context of your comment.</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <span  class='text-[16px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'> Media</span>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included.
+             Visitors to the website can download and extract any location data from images on the website.</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <span  class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.6px]'> Contact forms</span>
+       </div>
+
+       <div class='my-[25px]'>
+        <span  class='text-[16px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'>Cookies</span>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment.
+             These cookies will last for one year.</p>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If you visit our login page, we will set a temporary cookie to determine if your browser accepts cookies. 
+            This cookie contains no personal data and is discarded when you close your browser.</p>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> When you log in, we will also set up several cookies to save your login information and your screen display choices.
+            Login cookies last for two days, and screen options cookies last for a year. 
+            If you select “Remember Me”, your login will persist for two weeks. 
+            If you log out of your account, the login cookies will be removed.</p>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If you edit or publish an article, an additional cookie will be saved in your browser. 
+            This cookie includes no personal data and simply indicates the post ID of the article you just edited.
+            It expires after 1 day.</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <span  class='text-[16px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'> Embedded content from other websites</span>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>Articles on this site may include embedded content (e.g. videos, images, articles, etc.).
+           Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.</p>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> These websites may collect data about you, use cookies, embed additional third-party tracking,
+            and monitor your interaction with that embedded content, 
+            including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> Embedded content from other websites</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <span  class='text-[16px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'> Analytics </span>
+       
+       </div>
+
+       <div class='my-[25px]'>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px]'>Who we share your data with and How long we retain your data:</h1>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> Who we share your data with and How long we retain your data: If you leave a comment, the comment and its metadata are retained indefinitely. 
+            This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.</p>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> For users that register on our website (if any), we also store the personal information they provide in their user profile.
+             All users can see, edit, or delete their personal information at any time (except they cannot change their username). 
+            Website administrators can also see and edit that information.</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px]'> What rights you have over your data</h1>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If you have an account on this site, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you.
+            This does not include any data we are obliged to keep for administrative, legal, or security purposes.</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px]'>Where we send your data</h1>
+        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>  Visitor comments may be checked through an automated spam detection service.</p>
+       </div>
+
+       <div class='my-[25px]'>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'>Your contact information</h1>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'>Additional information</h1>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'> How we protect your data</h1>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'> What data breach procedures we have in place </h1>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'>What third parties we receive data from</h1>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'> What automated decision making and/or profiling we do with user data</h1>
+        <h1 class='text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]'>Industry regulatory disclosure requirements</h1>
+       </div>
+    </div>
+    <div class='h-[2px] bg-[#764A8733] mb-[150px]'></div>
 </div>
+
--- resources/views/livewire/term-comdition.blade.php
+++ resources/views/livewire/term-comdition.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+   sumon mia
 </div>
