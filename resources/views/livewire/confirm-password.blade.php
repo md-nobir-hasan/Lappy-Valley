cae495486214a48728d7fdbbda581cbcf36bb13e
Sumon-Mia <sumon.wdd@gmail.com>
2023-11-06 18:34:10 +0600

    Update confirm-password.blade.php

--- resources/views/livewire/confirm-password.blade.php
+++ resources/views/livewire/confirm-password.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <h1 class="text-[red]">I am Confirm password page</h1>
+<div class='container mx-auto'>
+    <div>
+        <h1 class='font-[jost] text-[16px] font-[400] leading-[25.3px] text-[#353535]'>Home / Account</h1>
+        <div class='h-[2px] bg-[#764A8733]'></div>
+    </div>
+
+    <div>
+        
+    </div>
 </div>
