2665d8f7c8998725a717fd2b0c7ef8035fcc5952
Sumon-Mia <sumon.wdd@gmail.com>
2023-11-12 10:43:04 +0600

    wishlist page done

--- resources/views/livewire/wishlist.blade.php
+++ resources/views/livewire/wishlist.blade.php
@@ -1,3 +1,64 @@
-<div>
-<h1 class="text-[red]">I am wishlislt page</h1>
+
+<div class='container mx-auto'>
+       <div class='flex justify-between'>
+       <h1 class='font-[jost] xl:text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Search / Lenovo</h1>
+       <div class='flex gap-[5px] items-center'><img src="/storage/product/vector-1.svg" alt="Product" class='w-[16px] h-[14px]'><span class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[23.12px]'>Log Out</span></div>
+       </div>
+        <div class='h-[2px] bg-[#764A8733]'></div>
+       <!-- --------------------user-name-------------------------------- -->
+       <div class='flex gap-[10px] items-center'>
+        <div class='w-[74px] h-[74px]'><img src="/storage/product/iconoir_profile-circle1.svg" alt="Product"></div>
+        <div>
+            <p class='text-[16px] text-[#000000] font-[Inter] font-[400] leadint-[14.52px]'>Hello,</p>
+            <h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leadint-[24.2px]'>Username Here</h1>
+        </div>
+       </div>
+
+       <div class='flex gap-[100px] my-[30px]'>
+        <div class='flex gap-[5px] items-center'>
+            <img src="/storage/product/icon-ad-product.svg"alt="Product" class='w-[23.36px] h-[26.21px]'>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Orders</span>
+        </div>
+        <div class='flex gap-[5px] items-center'>
+            <img src="/storage/product/iconoir_profile-circle.svg" alt="Product" class='w-[24px] h-[24px]'>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Edit Profile</span>
+        </div>
+        <div class='flex gap-[5px] items-center'>
+            <img src="/storage/product/carbon_password.svg" alt="Product" class='w-[24px] h-[24px]'>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Change Password</span>
+        </div>
+        <div class='flex gap-[5px] items-center'>
+            <img src="/storage/product/mdi_address-marker-outline.svg" alt="Product" class='w-[24px] h-[24px]'>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Address</span>
+        </div>
+        <div class='flex gap-[5px] items-center'>
+            <img src="/storage/product/Vector-2.svg" alt="Product" class='w-[22.3px] h-[20.88px]'>
+            <span class='text-[#380D37] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Wish List</span>
+        </div>
+       </div>
+       <div class='h-[2px] bg-[#764A8733]'></div>
+
+       <div>
+        <div><h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leading-24.2px[]'>Wish List:</h1></div>
+        <div class='bg-[#380D37] grid grid-cols-2 items-center gap-[50px] px-[20px] py-[15px]'>
+
+            <div class='flex gap-[50px]'>
+                <span class='text-[14px] text-[#FFFFFF] font-[jost] font-[500] leading-[20.23px]'>Image</span>
+                <span class='text-[14px] text-[#FFFFFF] font-[jost] font-[500] leading-[20.23px]'>Product Name</span>
+            </div>
+
+            <div class='right-0'>
+            <span class='text-[14px] text-[#FFFFFF] font-[jost] font-[500] leading-[20.23px]'>Price</span>
+            </div>
+        </div>
+        <div class='flex justify-between items-center px-[10px] py-[20px]'>
+           <div><img src="/storage/product/large-size-laptop.jpg" alt="Product" class='w-[48px] h-[48px]'></div>
+           <div><span class='text-[20px] text-[#000000] font-[jost] font-[500] leading-[28.9px]'>Lenovo IdeaPad 15AMN7 AMD Ryzen 5 7520U</span></div>
+           <div><span class='text-[20px] text-[#DC275C] font-[jost] font-[500] leading-[28.9px]'>1,50,000 ৳</span></div>
+           <div><button class='text-[14px] text-[#F2F2F2] font-[jost] font-[500] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C] w-[133px] h-[41px]'>Buy Now</button></div>
+           <div><img src="/storage/product/cross-icon.svg" alt="product"></div>
+        </div>
+       </div>
+       <div class='h-[1px] bg-[#380D37] mb-[100px]'></div>
+    
 </div>
