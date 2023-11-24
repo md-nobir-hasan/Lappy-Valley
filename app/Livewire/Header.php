89d42924e7f33a217d5edd1b16e2fe9d54c30d44
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-11-24 14:38:51 +0600

    live search by livewire

--- app/Livewire/Header.php
+++ app/Livewire/Header.php
@@ -11,6 +11,8 @@ class Header extends Component
 {
     public $cart_count = 0;
     public $products;
+    public $cats;
+    public $cat;
     public function mount()
     {
         $user = Auth::user();
@@ -19,8 +21,18 @@ class Header extends Component
         }else{
             $this->cart_count = Cart::where('ip', request()->ip())->get()->count();
         }
-        $this->products = DB::table('products')->select('id','photo','title', 'slug')->get();
+        $this->products = DB::table('products')->select('id','photo','title', 'slug','price', 'discount')->get();
+        $this->cats = DB::table('categories')->select('title','id')->orderBy('serial','asc')->get();
     }
+
+    public function prdouctFetch(){
+        if($id = $this->cat){
+            $this->products = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')->where('cat_id',$id)->get();
+        }else{
+            $this->products = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')->get();
+        }
+    }
+
     public function render()
     {
         return view('livewire.header');
