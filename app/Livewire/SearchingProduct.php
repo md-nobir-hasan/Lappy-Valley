b83c6715ebe5a8bab3c92ce23f27bf861288c53a
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-11-27 10:17:17 +0600

    pagination done

--- app/Livewire/SearchingProduct.php
+++ app/Livewire/SearchingProduct.php
@@ -13,32 +13,31 @@ class SearchingProduct extends Component
 {
     public $cat;
     public $stext;
-    public $slug_wise_product;
 
     public function mount()
     {
-        if (($id = $this->cat) && ($s = $this->stext)) {
-            $this->slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
-            ->where('cat_id', $id)
-                ->where('title', 'like', '%' . $this->stext . '%')
-                ->get();
-        } elseif (($id = $this->cat)) {
-            $this->slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
-            ->where('cat_id', $id)
-                ->get();
-        } elseif (($s = $this->stext)) {
-            $this->slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
-            ->where('title', 'like', '%' . $this->stext . '%')
-                ->get();
-        } else {
-            $this->slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
-            ->get();
-        }
     }
 
     public function render()
     {
-        return view('livewire.cat-wise-shop');
+        if (($id = $this->cat) && ($s = $this->stext)) {
+            $slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
+                ->where('cat_id', $id)
+                ->where('title', 'like', '%' . $this->stext . '%')
+                ->paginate(2);
+        } elseif (($id = $this->cat)) {
+            $slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
+                ->where('cat_id', $id)
+                ->paginate(2);
+        } elseif (($s = $this->stext)) {
+            $slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
+                ->where('title', 'like', '%' . $this->stext . '%')
+                ->paginate(2);
+        } else {
+            $slug_wise_product = DB::table('products')->select('id', 'photo', 'title', 'slug', 'price', 'discount')
+                ->paginate(2);
+        }
+        return view('livewire.cat-wise-shop', ['slug_wise_product' => $slug_wise_product]);
         // return view('livewire.searching-product');
     }
 }
