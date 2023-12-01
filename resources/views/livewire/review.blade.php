d729da6447b69b024dcfb56949dbb6ec8ba3597d
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-01 09:14:17 +0600

    not complete work

--- resources/views/livewire/review.blade.php
+++ resources/views/livewire/review.blade.php
@@ -1,53 +1,12 @@
-<div class="d-flex justify-content-center align-items-center vh-100">
-    	
-    <form class="shadow w-450 p-3" 
-          action="php/signup.php" 
-          method="post"
-          enctype="multipart/form-data">
-
-        <h4 class="display-4  fs-1">Create Account</h4><br>
-        <?php if(isset($_GET['error'])){ ?>
-        <div class="alert alert-danger" role="alert">
-          <?php echo $_GET['error']; ?>
-        </div>
-        <?php } ?>
-
-        <?php if(isset($_GET['success'])){ ?>
-        <div class="alert alert-success" role="alert">
-          <?php echo $_GET['success']; ?>
-        </div>
-        <?php } ?>
-      <div class="mb-3">
-        <label class="form-label">Full Name</label>
-        <input type="text" 
-               class="form-control"
-               name="fname"
-               value="<?php echo (isset($_GET['fname']))?$_GET['fname']:"" ?>">
-      </div>
-
-      <div class="mb-3">
-        <label class="form-label">User name</label>
-        <input type="text" 
-               class="form-control"
-               name="uname"
-               value="<?php echo (isset($_GET['uname']))?$_GET['uname']:"" ?>">
-      </div>
-
-      <div class="mb-3">
-        <label class="form-label">Password</label>
-        <input type="password" 
-               class="form-control"
-               name="pass">
-      </div>
-
-      <div class="mb-3">
-        <label class="form-label">Profile Picture</label>
-        <input type="file" 
-               class="form-control"
-               name="pp">
-      </div>
-      
-      <button type="submit" class="btn btn-primary">Sign Up</button>
-      <a href="login.php" class="link-secondary">Login</a>
-    </form>
+<div class="selection:bg-fuchsia-300 selection:text-fuchsia-900">
+  <p>
+    So I started to walk into the water. I won't lie to you boys, I was
+    terrified. But I pressed on, and as I made my way past the breakers
+    a strange calm came over me. I don't know if it was divine intervention
+    or the kinship of all living things but I tell you Jerry at that moment,
+    I <em>was</em> a marine biologist.
+  </p>
+  <button class="bg-blue-500 active:bg-blue-600 ...">
+    Submit
+  </button>
 </div>
