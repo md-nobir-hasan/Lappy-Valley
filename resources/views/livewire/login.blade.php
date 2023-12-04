7e81da22db8ef5b7d5e60ef8f27d095a94fd3ad3
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-04 19:29:07 +0600

    login and registration show and hide done

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,27 +1,30 @@
-    <div class="container mx-auto" x-data="dropdown()">
+    <div class="container mx-auto">
         <div class=>
             <h1 class='font-[jost] text-[16px] font-[400] leading-[25.3px] text-[#353535]'>Home / Account</h1>
             <div class='h-[2px] bg-[#764A8733]'></div>
         </div>
+
         <!-- --------------log-in--section----------- -->
 
         <div>
-            <div class=" flex gap-[10px] w-[463px] mx-auto">
-                <button type="button" onclick="login()"
-                    class="bg-[#F2F2F2] text-[16px] focus:outline-none focus:bg-[#380D37] focus:text-[#fff] text-[#380D37] font-[jost] font-[600] w-full text-center rounded-[5px]">LogIn</button>
-                {{-- <a href="{{route('user.login')}}">Log In</a> --}}
-                {{-- <a href="{{route('user.register')}}"> --}}
-                <button type="button"  onclick="reg()"
-                    class="bg-[#F2F2F2] text-[16px] focus:bg-[#380D37] focus:text-[#fff] text-[#380D37] font-[jost] font-[600] w-full py-[12px] text- rounded-[5px]">
+            <div class="flex gap-[10px] w-[463px] mx-auto">
+                <button type="button" id="login_btn"
+                    class="default text-[16px]  focus:outline-none bg-[#380D37] text-[#fff] font-[jost] font-[600] w-full text-center rounded-[5px]">
+                    LogIn</button>
+
+                <button type="button" id='reg_btn'
+                    class="change bg-[#F2F2F2] text-[#380D37] text-[16px] focus:text-[#fff]  font-[jost] font-[600] w-full py-[12px] text- rounded-[5px]">
                     Registration
                 </button>
-                {{-- </a> --}}
+
             </div>
 
             <div
                 class=" w-[463px] mx-auto border-[2px] border-[#380D37] rounded-[4px] left-[70px] pt-[33px] pb-[20px] pl-[65px] pr-[50px]">
-                <form wire:submit='login' class=" w-full mx-auto mt-[40px}" id='logInForm'>
-                    <div>
+                {{-- Login form  --}}
+                <div id='logInForm'>
+                    <form wire:submit='login' class="w-full mx-auto">
+                        {{-- <div> --}}
                         <div class="mb-[15px]">
                             <input wire:model.blur='email'
                                 class="italic rounded-[5px] bg-[#F2F2F2] py-[12px] pl-[20px] w-full font-[jost] font-[500]"
@@ -49,7 +52,6 @@
                                     Password?</a></div>
                         </div>
 
-
                         @if ($check_msg)
                             <span class="text-[red] text-[16px]">{{ $check_msg }}</span>
                         @endif
@@ -75,111 +77,99 @@
                                 Login
                             </button>
                         </div>
-                    </div>
-                </form>
-
-                <form wire:submit='save'
-                    class=" w-full  mt-[40px] hidden" id="regForm">
-              
-                    {{-- <div class="flex justify-between w-full">
-                        <a href="{{route('user.login')}}"> 
-                        <button type="button"
-                            class="bg-[#F2F2F2] focus:outline-none active:bg-[#380D37] focus:text-[#f2f2f2] text-[16px] text-[#380D37] font-[jost] font-[700] w-[232px]  py-[12px] text-center rounded-[5px]">Log
-                            In</button>
-                         </a> 
-                        <a href="{{route('user.register')}}">
-                        <button type="button"
-                            class="bg-[#F2F2F2] text-[16px] focus:bg-[#380D37] focus:text-[#f2f2f2] text-[#380D37] font-[jost] font-[700] py-[12px] w-[232px] text-center rounded-[5px]">
-                            Registration</button>
-                        </a>
-                    </div> --}}
-    
-                    <div >
+                        {{-- </div> --}}
+                    </form>
+                </div>
+                {{-- Registration form  --}}
+                <div id="regForm" class="hidden">
+                    <form wire:submit='save' class="w-full mt-[40px]">
                         <div class="mb-[10px]">
-                    <input wire:model.blur='email'
-                        class="italic rounded-[5px] bg-[#F2F2F2] py-[12px] w-full pl-[15px] text-[16px] text-[#353535] leading-[23.12px] font-[jost] font-[500]"
-                        type="email" placeholder="Enter Your Email Address">
-                    @error('email')
-                        <span class="text-[red] text-[12px]">{{ $message }}</span>
-                    @enderror
+                            <input wire:model.blur='email'
+                                class="italic rounded-[5px] bg-[#F2F2F2] py-[12px] w-full pl-[15px] text-[16px] text-[#353535] leading-[23.12px] font-[jost] font-[500]"
+                                type="email" placeholder="Enter Your Email Address">
+                            @error('email')
+                                <span class="text-[red] text-[12px]">{{ $message }}</span>
+                            @enderror
 
-                    <input name="password" wire:model.blur='password'
-                        class="italic my-[10px] rounded-[5px] bg-[#F2F2F2] py-[12px] text-[16px] text-[#353535] leading-[23.12px] w-full pl-[15px] font-[jost] font-[500]"
-                        type="password" placeholder="Set Password">
-                    @error('password')
-                        <span class="text-[red] text-[12px]">{{ $message }}</span>
-                    @enderror
+                            <input name="password" wire:model.blur='password'
+                                class="italic my-[10px] rounded-[5px] bg-[#F2F2F2] py-[12px] text-[16px] text-[#353535] leading-[23.12px] w-full pl-[15px] font-[jost] font-[500]"
+                                type="password" placeholder="Set Password">
+                            @error('password')
+                                <span class="text-[red] text-[12px]">{{ $message }}</span>
+                            @enderror
 
-                    <input name="confirmed" wire:model.blur='confirmed'
-                        class="italic my-[10px] rounded-[5px] bg-[#F2F2F2] py-[12px] text-[16px] text-[#353535] leading-[23.12px] w-full pl-[15px] font-[jost] font-[500]"
-                        type="password" placeholder="Confirm The Password">
+                            <input name="confirmed" wire:model.blur='confirmed'
+                                class="italic my-[10px] rounded-[5px] bg-[#F2F2F2] py-[12px] text-[16px] text-[#353535] leading-[23.12px] w-full pl-[15px] font-[jost] font-[500]"
+                                type="password" placeholder="Confirm The Password">
 
-                    @error('confirmed')
-                        <span class="text-[red] text-[12px]">{{ $message }}</span>
-                    @enderror
+                            @error('confirmed')
+                                <span class="text-[red] text-[12px]">{{ $message }}</span>
+                            @enderror
 
-                    <div class="my-[10px]">
-                        <button
-                            class="font-[jost] font-[500] text-[16px] text-[#f2f2f2] bg-gradient-to-r from-[#380D37] to-[#DC275C] py-[12px] w-full rounded-[5px] flex justify-center items-center">
-                            <div wire:loading
-                                class="inline-block h-6 w-6 mr-2 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-success motion-reduce:animate-[spin_1.5s_linear_infinite]"
-                                role="status">
-                                <span
-                                    class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...
-                                </span>
+                            <div class="my-[10px]">
+                                <button
+                                    class="font-[jost] font-[500] text-[16px] text-[#f2f2f2] bg-gradient-to-r from-[#380D37] to-[#DC275C] py-[12px] w-full rounded-[5px] flex justify-center items-center">
+                                    <div wire:loading
+                                        class="inline-block h-6 w-6 mr-2 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-success motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                                        role="status">
+                                        <span
+                                            class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...
+                                        </span>
+                                    </div>
+                                    Register
+                                </button>
                             </div>
-                            Register
-                        </button>
-                    </div>
-                    <div class='flex justify-evenly items-center gap-2 my-[25px]'>
-                        <div class='h-[1px] w-[155px] bg-[#000000]'></div>
-                        <div class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[20.23px]'>OR</div>
-                        <div class='h-[1px] w-[155px] bg-[#000000]'></div>
-                    </div>
-                    <div class='items-center my-[25px]'>
-                        <div
-                            class='my-[25px] border-[1px] border-[#380D37] rounded-[4px] flex items-center justify-center'>
-                            <a href="#"><button
-                                    class=' h-[44px] flex gap-2 items-center justify-center text-[16px] text-[#380D37] font-[jost] font-[500] leading-[23.12px]'>Log
-                                    in with <img src="/storage/product/google.svg" alt="Product"></button></a>
+                            <div class='flex justify-evenly items-center gap-2 my-[25px]'>
+                                <div class='h-[1px] w-[155px] bg-[#000000]'></div>
+                                <div class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[20.23px]'>OR
+                                </div>
+                                <div class='h-[1px] w-[155px] bg-[#000000]'></div>
+                            </div>
+                            <div class='items-center my-[25px]'>
+                                <div
+                                    class='my-[25px] border-[1px] border-[#380D37] rounded-[4px] flex items-center justify-center'>
+                                    <a href="#"><button
+                                            class=' h-[44px] flex gap-2 items-center justify-center text-[16px] text-[#380D37] font-[jost] font-[500] leading-[23.12px]'>Log
+                                            in with <img src="/storage/product/google.svg" alt="Product"></button></a>
+                                </div>
+                                <div
+                                    class='my-[25px] border-[1px] border-[#380D37] rounded-[4px] flex items-center justify-center'>
+                                    <a href="#"><button
+                                            class=' h-[44px] flex gap-2 items-center justify-center text-[16px] text-[#380D37] font-[jost] font-[500] leading-[23.12px]'>Log
+                                            in with <img src="/storage/product/facebook.svg"
+                                                alt="Product"></button></a>
+                                </div>
+                            </div>
                         </div>
-                        <div
-                            class='my-[25px] border-[1px] border-[#380D37] rounded-[4px] flex items-center justify-center'>
-                            <a href="#"><button
-                                    class=' h-[44px] flex gap-2 items-center justify-center text-[16px] text-[#380D37] font-[jost] font-[500] leading-[23.12px]'>Log
-                                    in with <img src="/storage/product/facebook.svg" alt="Product"></button></a>
-                        </div>
-                    </div>
+                    </form>
+                </div>
             </div>
-
-            </form>
         </div>
     </div>
-    </div>
-
-
-
 
 
     <script>
-        var logIn = document.getElementById('logInForm');
-        var regF = document.getElementById('regForm');
-   
-        function login() {
-            regForm.style.display = 'none';
-            logInForm.style.display = 'block';
-        }
+        $(document).ready(function() {
+            
+            $('#login_btn').on('click', function() {
+                $('#regForm').hide();
+                $('#logInForm').show();
+                $(this).addClass('bg-[#380D37] text-[#fff]');
+                $(this).removeClass('bg-[#F2F2F2] text-[#380D37]');
+                $('#reg_btn').removeClass('bg-[#380D37] text-[#fff]')
+                $('#reg_btn').addClass('bg-[#F2F2F2] text-[#380D37]');
+            });
 
-        function reg() {
-            regForm.style.display = 'block';
-            logInForm.style.display = 'none';
-        }
-
-   
+            $('#reg_btn').on('click', function() {
+                $('#logInForm').hide();
+                $('#regForm').show();
+                $(this).addClass('bg-[#380D37] text-[#fff]');
+                $(this).removeClass('bg-[#F2F2F2] text-[#380D37]');
+                $('#login_btn').removeClass('bg-[#380D37] text-[#fff]');
+                $('#login_btn').addClass('bg-[#F2F2F2] text-[#380D37]');
+            });
+        })
     </script>
-
-
-
 
 
     </div>
--- resources/views/livewire/review.blade.php
+++ resources/views/livewire/review.blade.php
@@ -1,9 +1,37 @@
-<div class="account-page">
-  <div class="container">
-    <div class="row">
-      <div class="col-2">
-       
-      </div>
-    </div>
-  </div>
+<div class="container ">
+  
+  <style>
+    /* Define default styles for the buttons */
+    .default-button {
+      background-color: #3498db;
+      color: #ffffff;
+      padding: 10px 20px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .other-button {
+      background-color: #2ecc71;
+      color: #ffffff;
+      padding: 10px 20px;
+      border: none;
+      cursor: pointer;
+    } 
+  </style>
+
+
+  <!-- HTML buttons with onclick event -->
+  <button class="default-button" onclick="changeBackground()">Default Button</button>
+  <button class="other-button" onclick="changeBackground()">Other Button</button>
+
+  <!-- JavaScript to handle button click event and change background color -->
+  <script>
+    function changeBackground() {
+      // Select the default button and change its background color
+      var defaultButton = document.querySelector('.default-button');
+      defaultButton.style.backgroundColor = '#e74c3c'; // Change to your desired color
+    }
+  </script>
 </div>
+
+
