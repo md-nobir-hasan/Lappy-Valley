579616abf6c46f1f601914174d0032a8259a933f
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-04 13:34:08 +0600

    about us half done

--- resources/views/livewire/about-us.blade.php
+++ resources/views/livewire/about-us.blade.php
@@ -1,3 +1,27 @@
-<div>
-    I am about us page
+<div class="container mx-auto">
+    <div class=''>
+        <h1 class='font-[jost] text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Home / About Us</h1>
+    </div>
+         <div class='h-[2px] bg-[#764A8733]'></div>
+
+         <div class="grid grid-cols-3 gap-4 items-center ">
+            <div class="items-center flex flex-col ">
+                <div><img src="/storage/product/A-Rohman.png" alt=""></div>
+                
+                    <div><h1 class="font-[jost] font-[700] text-[20px] text-[#353535]">Mohammad A Rahman</h1></div>
+                <div><p class="font-[jost] font-[400] text-[16px] text-[#f78da7]">Entrepreneur</p></div>
+               
+            </div>
+            <div>
+                <div><img src="/storage/product/Haque.jpg" alt=""></div>
+                <div><h1 class="font-[jost] font-[700] text-[20px] text-[#353535]">Kazi Mazharul Haque</h1></div>
+                <div><p class="font-[jost] font-[700] text-[16px] text-[#f78da7]">Entrepreneur</p></div>
+            </div>
+            <div>
+                <div><img src="/storage/product/mostafizur-rohman.jpeg" alt=""></div>
+                <div><h1 class="font-[jost] font-[700] text-[20px] text-[#353535]">A.K.M. Mostafizur Rahman</h1></div>
+                <div><p class="font-[jost] font-[700] text-[16px] text-[#f78da7] items-center">Entrepreneur</p></div>
+            </div>
+         </div>
 </div>
+        
