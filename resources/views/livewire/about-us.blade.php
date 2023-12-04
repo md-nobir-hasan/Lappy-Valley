7e7685140ad6460c154bab3afb172e8f15600753
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-04 14:06:25 +0600

    about us page done

--- resources/views/livewire/about-us.blade.php
+++ resources/views/livewire/about-us.blade.php
@@ -2,26 +2,53 @@
     <div class=''>
         <h1 class='font-[jost] text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Home / About Us</h1>
     </div>
-         <div class='h-[2px] bg-[#764A8733]'></div>
+    <div class='h-[2px] bg-[#764A8733]'></div>
 
-         <div class="grid grid-cols-3 gap-4 items-center ">
-            <div class="items-center flex flex-col ">
-                <div><img src="/storage/product/A-Rohman.png" alt=""></div>
-                
-                    <div><h1 class="font-[jost] font-[700] text-[20px] text-[#353535]">Mohammad A Rahman</h1></div>
-                <div><p class="font-[jost] font-[400] text-[16px] text-[#f78da7]">Entrepreneur</p></div>
-               
+    <div class="grid grid-cols-3 gap-4 items-center my-4">
+        <div class="items-center flex flex-col gap-4">
+            <div><img src="/storage/product/A-Rohman.png" alt=""></div>
+
+            <div>
+                <h1 class="font-[jost] font-[700] text-[24px] text-[#353535]">Mohammad A Rahman</h1>
             </div>
             <div>
-                <div><img src="/storage/product/Haque.jpg" alt=""></div>
-                <div><h1 class="font-[jost] font-[700] text-[20px] text-[#353535]">Kazi Mazharul Haque</h1></div>
-                <div><p class="font-[jost] font-[700] text-[16px] text-[#f78da7]">Entrepreneur</p></div>
+                <p class="font-[jost] font-[400] text-[18px] text-[#f78da7]">Entrepreneur</p>
+            </div>
+
+        </div>
+        <div class="items-center flex flex-col gap-4">
+            <div><img src="/storage/product/Haque.jpg" alt=""></div>
+            <div>
+                <h1 class="font-[jost] font-[700] text-[24px] text-[#353535]">Kazi Mazharul Haque</h1>
             </div>
             <div>
-                <div><img src="/storage/product/mostafizur-rohman.jpeg" alt=""></div>
-                <div><h1 class="font-[jost] font-[700] text-[20px] text-[#353535]">A.K.M. Mostafizur Rahman</h1></div>
-                <div><p class="font-[jost] font-[700] text-[16px] text-[#f78da7] items-center">Entrepreneur</p></div>
+                <p class="font-[jost] font-[700] text-[18px] text-[#f78da7]">Entrepreneur</p>
             </div>
-         </div>
+        </div>
+        <div class="items-center flex flex-col gap-4">
+            <div><img src="/storage/product/mostafizur-rohman.jpeg" alt=""></div>
+            <div>
+                <h1 class="font-[jost] font-[700] text-[24px] text-[#353535]">A.K.M. Mostafizur Rahman</h1>
+            </div>
+            <div>
+                <p class="font-[jost] font-[700] text-[18px] text-[#f78da7] items-center">Entrepreneur</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="my-[100px] flex flex-col text-left gap-8 w-[900px] mx-auto">
+        <div class="font-[jost] font-[700] text-[#353535] text-[24px]">
+            <h1>About</h1>
+        </div>
+        <div class="text-[18px] text-[#380535] font-[jost] font-[500] flex flex-col gap-2">
+            <p>
+                Tread Lisence Certificate No: 202984</p>
+            <p>Bin Number : 0049818790102</p>
+        </div>
+        <div class="text-[18px] text-[#380535] font-[jost] font-[400]">
+            <p>
+                Brand New and used Laptop import from USA, Dubai & Japan. MacBook, HP, ASUS, Dell, Lenovo, MSI, Acer, Microsoft Surface, Toshiba, Razer Blade & Samsung laptop are available in different models with a very competitive market price.
+            </p>
+        </div>
+    </div>
 </div>
-        
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,4 +1,4 @@
-    <div class="container mx-auto" x-data="dropdown()">>
+    <div class="container mx-auto" x-data="dropdown()">
         <div class=>
             <h1 class='font-[jost] text-[16px] font-[400] leading-[25.3px] text-[#353535]'>Home / Account</h1>
             <div class='h-[2px] bg-[#764A8733]'></div>
