e1eeea1e1780c1f35daa0d9d25e550649addc5d1
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-10 19:07:49 +0600

    wish list done

--- resources/views/livewire/wishlist.blade.php
+++ resources/views/livewire/wishlist.blade.php
@@ -1,6 +1,8 @@
-<div class='px-[100px] max-md:px-[45px] max-sm:px-[20px] mx-auto max-sm:mt-[70px] max-xl:mt-[100px]'>
+<div class='px-[100px] max-md:px-[45px] max-lg:px-[70px] max-sm:px-[15px] mx-auto max-sm:mt-[70px] max-xl:mt-[100px]'>
     <div class='flex justify-between'>
-        <h1 class='font-[jost] xl:text-[20px] max-sm:text-[16px] max-md:text-[] max-lg:text-[] font-[400] leading-[25.3px] text-[#353535]'>Search / Lenovo</h1>
+        <h1
+            class='font-[jost] xl:text-[20px] max-sm:text-[16px] max-md:text-[] max-lg:text-[] font-[400] leading-[25.3px] text-[#353535]'>
+            Search / Lenovo</h1>
         <div class='flex gap-[5px] items-center'><img src="/storage/product/vector-1.svg" alt="Product"
                 class='w-[16px] h-[14px]'><span
                 class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[23.12px]'>Log Out</span></div>
@@ -8,21 +10,70 @@
     <div class='h-[2px] bg-[#764A8733]'></div>
     <!-- --------------------user-name-------------------------------- -->
     <div class='flex gap-[10px] items-center my-[20px]'>
-        <div class='w-[74px] h-[74px]'>
+        <style>
+            @media (max-width:530px) {
+               .hello {
+                width: 55px;
+                height: 55px;
+               }
+               .hello-0{
+                font-size:10px;
+               }
+               .hello-1{
+                font-size:14px;
+               }
+               .now{
+                width:60px;
+                height:60px;
+                display: none;
+               }
+               .amd{
+                font-size: 10px;
+               }
+               .pay{
+                font-size: 10px;
+               }
+               .product-4{
+                width: 370px;
+               }
+               .cros{
+                width: 18px;
+                height: 18px;
+               }
+               .laptop-s{
+                width:35px;
+                height:35px;
+               }
+               .amd{
+                line-height: 0px;
+               }
+               .head-td{
+                font-size: 10px
+               }
+
+            }
+            @media(min-width:530px){
+                .now-1{
+                    display: none;
+                }
+            }
+        </style>
+        <div class='hello w-[74px] h-[74px]'>
             <img src="/storage/product/iconoir_profile-circle1.svg" alt="Product">
         </div>
         <div>
-            <p class='text-[16px] max-sm:text-[14px] text-[#000000] font-[Inter] font-[400] leadint-[14.52px]'>
+            <p class='hello-0 text-[16px] max-sm:text-[14px] text-[#000000] font-[Inter] font-[400] leadint-[14.52px]'>
                 Hello,
             </p>
-            <h1 class='text-[20px] max-sm:text-[16px] max-md:text-[] max-lg:text-[] text-[#000000] font-[Inter] font-[500] leadint-[24.2px]'>
+            <h1
+                class='username text-[20px] max-sm:text-[16px] max-md:text-[] max-lg:text-[] text-[#000000] font-[Inter] font-[500] leadint-[24.2px]'>
                 Username Here
             </h1>
         </div>
     </div>
 
     <div
-        class='grid grid-cols-5 gap-[20px] max-sm:grid-cols-2 max-lg:grid-cols-3 max-lg:w-[500px] w-[1000px] my-[30px]'>
+        class='product-4 grid grid-cols-5 gap-[20px] max-sm:grid-cols-2 max-lg:grid-cols-3 max-lg:w-[510px] w-[1000px] my-[30px]'>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/icon-ad-product.svg"alt="Product" class='w-[23.36px] h-[26.21px]'>
             <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>
@@ -58,49 +109,77 @@
 
 
     <div class='my-[15px]'>
-        <h1 class='text-[20px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#000000] font-[Inter] font-[500] leading-24.2px[]'>
+        <h1
+            class='text-[20px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#000000] font-[Inter] font-[500] leading-24.2px[]'>
             Wish List:
         </h1>
     </div>
     <table class="w-full">
         <thead>
             <tr class="bg-[#380D37]">
-                <td class='py-3 px-6 max-sm:py-[10px] max-sm:pr-[0px] max-sm:pl-[4px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>Image</td>
-                <td class='py-3 px-6 max-sm:py-[10px] max-sm:pr-[0px] max-sm:pl-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>Product Name</td>
-                <td class='py-3 px-6 max-sm:py-[10px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>Price</td>
-                <td class='text-[14px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'></td>
-                <td class='text-[14px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'></td>
+                <td
+                    class='head-td flex items-center py-3 px-6 max-sm:py-[10px] max-sm:pr-[0px] max-sm:pl-[4px] max-md:py-[] max-md:pl-[8px] max-md:pr-[0px]
+                 max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>
+                    Image
+                </td>
+                <td
+                    class='head-td items-center py-3 px-6 max-sm:py-[10px] max-sm:pr-[0px] max-sm:pl-[0px] max-md:py-[0px] max-md:pl-[0px] max-md:pr-[0px] 
+                max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[14px] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>
+                    Product Name
+                </td>
+                <td
+                    class='head-td items-center py-3 px-6 max-sm:py-[10px] max-sm:px-[0px] max-md:py-[0px] max-md:pl-[0px] max-md:pr-[0px] 
+                max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>
+                    Price
+                </td>
+                <td
+                    class='text-[14px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>
+                </td>
+                <td
+                    class='text-[14px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>
+                </td>
             </tr>
         </thead>
 
         <tbody class='items-center'>
-            <td class="py-3 px-6 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
-                <img src="/storage/product/large-size-laptop.jpg" alt="Product"
-                    class='w-[48px] h-[48px]'>
-                </td>
-            <td class="py-3 px-6 max-sm:py-[2px] max-sm:pr-[0px] max-sm:pl-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+            <td
+                class="py-3 px-6 max-sm:py-[0px] max-sm:px-[0px] max-md:py-[] max-md:pl-[8px] max-md:pr-[0px] max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[]">
+                <img src="/storage/product/large-size-laptop.jpg" alt="Product" class='laptop-s w-[48px] h-[48px] max-sm:w-[40px] max-sm:h-[40px]'>
+            </td>
+            <td
+                class="py-3 px-6 max-sm:py-[0px] max-sm:pr-[0px] max-sm:pl-[0px] max-md:py-[0px] max-md:pl-[0px] max-md:pr-[0px] max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[]">
                 <span
-                    class='text-[20px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#000000] font-[jost] font-[500] leading-[28.9px]'>
+                    class='amd text-[20px] max-sm:text-[10px] max-md:text-[12px] max-lg:text-[14px] text-[#000000] font-[jost] font-[500] leading-[28.9px]'>
                     Lenovo IdeaPad 15AMN7 AMD Ryzen 5 7520U
                 </span>
             </td>
-            <td class="py-3 px-6 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+            <td
+                class="py-3 px-6 max-sm:py-[0px] max-sm:px-[0px] max-md:py-[0px] max-md:pl-[0px] max-md:pr-[0px] max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[]">
                 <span
-                    class='text-[20px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#DC275C] font-[jost] font-[700] leading-[28.9px]'>
+                    class='pay text-[20px] max-sm:text-[10px] max-md:text-[14px] max-lg:text-[14px] text-[#DC275C] font-[jost] font-[700] leading-[28.9px]'>
                     1,50,000 ৳
                 </span>
             </td>
-            <td class="py-3 px-6 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+            <td
+                class="py-3 px-6 max-sm:py-[0px] max-sm:px-[0px] max-md:py-[0px] max-md:pl-[0px] max-md:pr-[0px] max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[]">
                 <button
-                    class='text-[14px] max-sm:text-[10px] max-md:text-[] max-lg:text-[] text-[#F2F2F2] font-[jost] font-[500] rounded-[4px] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C] w-[133px] max-sm:w-[70px] max-sm:h-[25px] h-[41px]'>
+                    class='now text-[14px] max-sm:text-[10px] max-md:text-[12px] max-lg:text-[] text-[#F2F2F2] font-[jost] font-[500] rounded-[4px] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C] 
+                    w-[133px] max-sm:w-[70px] max-sm:h-[25px] max-md:w-[80px] max-md:h-[30px] max-lg:w-[100px] max-lg:h-[35px] h-[41px]'>
                     Buy Now
                 </button>
             </td>
-            <td class="py-3 px-2 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
-                <img src="/storage/product/cross-icon.svg" class="w-[28px] h-[28px] text-[#000000]" alt="product">
+            <td
+                class="py-3 px-2 max-sm:py-[0px] max-sm:px-[0px] max-md:py-[0px] max-md:pl-[0px] max-md:pr-[0px] max-lg:py-[12px] max-lg:pl-[8px] max-lg:pr-[0px] max-xl:py-[] max-xl:px-[]">
+                <img src="/storage/product/cross-icon.svg" class="cros w-[28px] h-[28px] max-sm:w-[25px] max-sm:h-[25px] text-[#000000]" alt="product">
             </td>
         </tbody>
     </table>
     <div class='h-[1px] bg-[#380D37]'></div>
-    <div class="h-[2px] bg-[#764A8733] mt-[200px] "></div>
+
+    <button
+    class='now-1 mt-[10px] text-[14px] max-sm:text-[10px] max-md:text-[12px] max-lg:text-[] text-[#F2F2F2] font-[jost] font-[500] rounded-[4px] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C] 
+    w-[133px] max-sm:w-[70px] max-sm:h-[25px] max-md:w-[80px] max-md:h-[30px] max-lg:w-[100px] max-lg:h-[35px] h-[41px]'>
+    Buy Now
+</button>
+    <div class="h-[2px] bg-[#764A8733] mt-[200px] max-sm:mt-[50px] "></div>
 </div>
