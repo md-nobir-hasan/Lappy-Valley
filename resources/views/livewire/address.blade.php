5fd6111260f42aec8655cf6c0377543dbc4c71e9
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-10 15:38:42 +0600

    address

--- resources/views/livewire/address.blade.php
+++ resources/views/livewire/address.blade.php
@@ -1,20 +1,20 @@
-
-<div class='container mx-auto'>
-       <div class='flex justify-between'>
-       <h1 class='font-[jost] xl:text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Home / Account</h1>
-       <div class='flex gap-[5px] items-center'><img src="/storage/product/vector-1.svg" alt="Product" class='w-[16px] h-[14px]'><span class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[23.12px]'>Log Out</span></div>
-       </div>
-        <div class='h-[2px] bg-[#764A8733]'></div>
-       <!-- --------------------user-name-------------------------------- -->
-       <div class='flex gap-[10px] items-center mt-[30px]'>
+<div class='px-[100px] max-md:px-[45px] max-sm:px-[20px] mx-auto max-sm:mt-[70px] max-xl:mt-[100px]'>
+    <div class='flex justify-between'>
+        <h1 class='font-[jost] xl:text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Home / Account</h1>
+        <div class='flex gap-[5px] items-center'><img src="/storage/product/vector-1.svg" alt="Product"
+                class='w-[16px] h-[14px]'><span
+                class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[23.12px]'>Log Out</span></div>
+    </div>
+    <div class='h-[2px] bg-[#764A8733]'></div>
+    <!-- --------------------user-name-------------------------------- -->
+    <div class='flex gap-[10px] items-center mt-[30px]'>
         <div class='w-[74px] h-[74px]'><img src="/storage/product/iconoir_profile-circle1.svg" alt="Product"></div>
         <div>
             <p class='text-[16px] text-[#000000] font-[Inter] font-[400] leadint-[14.52px]'>Hello,</p>
             <h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leadint-[24.2px]'>Username Here</h1>
         </div>
-       </div>
-
-       <div class='flex gap-[100px] my-[30px]'>
+    </div>
+    <div class='grid grid-cols-5 gap-[20px] max-sm:grid-cols-2 max-lg:grid-cols-3 max-lg:w-[500px] w-[1000px] my-[30px]'>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/icon-ad-product.svg"alt="Product" class='w-[23.36px] h-[26.21px]'>
             <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Orders</span>
@@ -35,49 +35,73 @@
             <img src="/storage/product/Vector-light-opacity.svg" alt="Product" class='w-[22.3px] h-[20.88px]'>
             <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Wish List</span>
         </div>
-       </div>
-       <div class='h-[2px] bg-[#764A8733]'></div>
-
-       <div class='mt-[20px]'>
-        <h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leading-[24.2px]'>Address:</h1>
-       </div>
-
-
-        <div class='flex gap-[50px] justify-start'>
-        <div>
-        <div class='my-[15px]'>
-        <label for="address" class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] block my-[5px]'>Address:</label>
-            <input type="email" placeholder='Enter the address' class='w-[344px] h-[44px] bg-[#F2F2F2] font-[jost] font-[500] text-[16px] text-[#353535] block italic px-[10px] rounded-[4px]'>
+    </div>
+    <div class='h-[2px] bg-[#764A8733]'></div>
+    <div class='flex flex-col max-lg:border-[2px] max-lg:border-[#353535] max-lg:w-[400px] max-lg:mx-auto max-lg:py-[20px] max-lg:pl-[40px] max-sm:pl-[30px] max-lg:rounded-[4px] mt-[20px] gap-[20px]'>
+        <div class=''>
+            <h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leading-[24.2px]'>Address:</h1>
         </div>
-        <div class='my-[15px]'>
-        <label for="address" class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] block my-[5px]'>City:</label>
-            <input type="email" placeholder='Enter the city Name' class='w-[344px] h-[44px] bg-[#F2F2F2] font-[jost] font-[500] text-[16px] text-[#353535]  blcok italic px-[10px] rounded-[4px]'>
+      <div class="flex gap-[40px] items-center">
+        <div class=''>
+            <label for="address"
+                class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] block my-[5px]'>Address:</label>
+            <input type="email" placeholder='Enter the address'
+                class='w-[344px] h-[44px] bg-[#F2F2F2] font-[jost] font-[500] text-[16px] text-[#353535] block italic px-[10px] rounded-[4px]'>
+        </div>
+        <div class='max-lg:hidden translate-y-[20px]'>
+            <div>
+                <h1 class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>
+                    Is this the default address?</h1>
             </div>
-        <div class='my-[15px]'>
-            <label for="address" class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] block my-[5px]'>Post code:</label>
-            <input type="email" placeholder='Enter the post code here' class='w-[344px] h-[44px] bg-[#F2F2F2] font-[jost] font-[500] text-[16px] text-[#353535]  block italic px-[10px] rounded-[4px]'>
+            <div class="flex items-center gap-2">
+                <input type="radio" class='w-[18px] h-[18px] accent-[#380D37]'>
+                <label for="text"
+                    class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>Yes.</label>
+            </div>
+            <div class="flex items-center gap-2">
+                <input type="radio" class='w-[18px] h-[18px] accent-[#380D37]'>
+                <label for="text"
+                    class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>No.</label>
+            </div>
         </div>
-        <div class='my-[30px]'>
-            <button class='w-[133px] h-[41px] rounded-[4px] text-[#F2F2F2] text-[14px] font-[jost] font-[500] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C]'>Save Changes</button>
+      </div>
+        <div class=''>
+            <label for="address"
+                class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] block my-[5px]'>City:</label>
+            <input type="email" placeholder='Enter the city Name'
+                class='w-[344px] h-[44px] bg-[#F2F2F2] font-[jost] font-[500] text-[16px] text-[#353535]  blcok italic px-[10px] rounded-[4px]'>
         </div>
+        <div class=''>
+            <label for="address"
+                class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] block my-[5px]'>Post
+                code:</label>
+            <input type="email" placeholder='Enter the post code here'
+                class='w-[344px] h-[44px] bg-[#F2F2F2] font-[jost] font-[500] text-[16px] text-[#353535]  block italic px-[10px] rounded-[4px]'>
         </div>
+        <div class=''>
+            <button
+                class='w-[133px] h-[41px] rounded-[4px] text-[#F2F2F2] text-[14px] font-[jost] font-[500] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C]'>Save
+                Changes</button>
+        </div>
+        <div class='hidden max-lg:block'>
+            <div>
+                <h1 class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>
+                    Is this the default address?</h1>
+            </div>
+            <div class="flex items-center gap-2">
+                <input type="radio" class='w-[18px] h-[18px] accent-[#380D37]'>
+                <label for="text"
+                    class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>Yes.</label>
+            </div>
+            <div class="flex items-center gap-2">
+                <input type="radio" class='w-[18px] h-[18px] accent-[#380D37]'>
+                <label for="text"
+                    class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>No.</label>
+            </div>
+        </div>
+    </div>
 
-        <div class='mt-[20px]'>
-           <div>
-            <h1 class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>Is this the default address?</h1>
-           </div>
-           <div class="flex items-center gap-2">
-            <input type="radio" class='w-[18px] h-[18px] accent-[#380D37]'>
-            <label for="text" class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>Yes.</label>
-           </div>
-           <div class="flex items-center gap-2">
-            <input type="radio" class='w-[18px] h-[18px] accent-[#380D37]'>
-            <label for="text" class='text-[16px] text-[#353535] font-[jost] font-[500] leading-[23.12px] tracking-[3%] my-[5px]'>No.</label>
-           </div>
-       </div>
-       </div>
 
-      
-       <div class='h-[1px] bg-[#764A8733] my-[20px]'></div>
-    
+    <div class='h-[1px] bg-[#764A8733] my-[20px]'></div>
+
 </div>
