af2b02fdffca460a85cd0a7fa60049456b4663f9
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-10 09:14:04 +0600

    terms & condition done

--- resources/views/livewire/term-comdition.blade.php
+++ resources/views/livewire/term-comdition.blade.php
@@ -1,90 +1,132 @@
-<div class='container mx-auto'>
-<div class=>
-            <h1 class='font-[jost] text-[16px] font-[400] tracking-[.48px] text-[#353535]'>Home / Terms & Conditions
-        </div>
-        <div class='h-[2px] bg-[#764A8733]'></div>
+<div class='container mx-auto overflow-hidden'>
+    <div class='max-sm:mt-[70px] max-xl:mt-[100px]'>
+        <h1 class='font-[jost] text-[16px] max-sm:text-[14px] font-[400] tracking-[.48px] text-[#353535]'>Home / Terms & Conditions
+    </div>
+    <div class='h-[2px] bg-[#764A8733]'></div>
 
-     <div class='text-center mt-[40px] mb-[50px]'>
-        <h2 class='text-[#380D37] text-[36px] font-[jost] font-[700]'>Terms & Conditions</h2>
+    <div class='text-center mt-[40px] mb-[50px] max-sm:mt-[16px] max-sm:mb-[20px]'>
+        <h2 class='text-[#380D37] text-[36px] max-sm:text-[30px] font-[jost] font-[700]'>Terms & Conditions</h2>
     </div>
 
-    <div class='text-left w-[1030px] mx-auto'>
-       <div class='my-[25px]'>
-        <h1 class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.6px]'> Dear Customer,</h1>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> The Return and Refund policy of Lappy Valley are given below –</br>ল্যাপী ভ্যালি এর রিটার্ন ও রিফান্ড পলিসি নিম্নরূপ –</p>
-       </div>
+    <div class='text-left flex flex-col'>
+        <div class='my-[0px]'>
+            <h1 class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.6px]'> Dear Customer,</h1>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> The Return and
+                Refund policy of Lappy Valley are given below –</br>ল্যাপী ভ্যালি এর রিটার্ন ও রিফান্ড পলিসি নিম্নরূপ –
+            </p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>We deliver within 5 days inside Dhaka and 10 days outside Dhaka</p>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>We deliver within 5
+                days inside Dhaka and 10 days outside Dhaka</p>
 
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>আমরা ঢাকার ভিতরে ৫ দিনের মধ্যে এবং ঢাকার বাইরে ১০ দিনের মধ্যে ডেলিভারি করি</p>
-       </div>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>আমরা ঢাকার ভিতরে ৫
+                দিনের মধ্যে এবং ঢাকার বাইরে ১০ দিনের মধ্যে ডেলিভারি করি</p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If a customer is buying the products from our shops, then please make sure to check the products in front of our Sales Executive. Later, if any problems occurs than the customer will not be entitled to any
-          changes but will be given services based on the products provided description.(Warranty)</p>
-          <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>আউট লেট থেকে পণ্য ক্রয়ের ক্ষেত্রে অবশ্যই শপে বিক্রয়কর্মীর সামনে চেক করে কিনবেন। 
-            পরবর্তীতে সমস্যা হলে যদি পণ্যে ওয়ারেন্টি থাকে তবে তা ওয়ারেন্টির আওতাভুক্ত হবে।</p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If a customer is
+                buying the products from our shops, then please make sure to check the products in front of our Sales
+                Executive. Later, if any problems occurs than the customer will not be entitled to any
+                changes but will be given services based on the products provided description.(Warranty)</p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>আউট লেট থেকে পণ্য
+                ক্রয়ের ক্ষেত্রে অবশ্যই শপে বিক্রয়কর্মীর সামনে চেক করে কিনবেন।
+                পরবর্তীতে সমস্যা হলে যদি পণ্যে ওয়ারেন্টি থাকে তবে তা ওয়ারেন্টির আওতাভুক্ত হবে।</p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p  class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>In the case of ordering online, after receiving the product, if any manufacturing issues or problems are noticed or discovered than customer has to inform us within 24 hours via our hotline service.</p>
-        <p  class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>অনলাইন অর্ডারের ক্ষেত্রে পণ্য ডেলিভারি পাবার পর পণ্যে মেনুফেকচারিং ত্রুটি থাকলে আমাদের হটলাইনে ২৪ ঘন্টার মধ্যে জানাতে হবে।
-        </p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>In the case of
+                ordering online, after receiving the product, if any manufacturing issues or problems are noticed or
+                discovered than customer has to inform us within 24 hours via our hotline service.</p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>অনলাইন অর্ডারের
+                ক্ষেত্রে পণ্য ডেলিভারি পাবার পর পণ্যে মেনুফেকচারিং ত্রুটি থাকলে আমাদের হটলাইনে ২৪ ঘন্টার মধ্যে জানাতে
+                হবে।
+            </p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If a customer received a manufacturing defected product than the customer has to visit any of our shops where our specialists will review the product first and then take the necessary steps to changing the product if it is in need of replacement.
-        </p>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>ত্রুটিযুক্ত পণ্য আমাদের স্টোর থেকে পরিবর্তন করা যাবে। এক্ষেত্রে আমাদের এক্সপার্টগন পণ্যে ত্রুটি পর্যবেক্ষণ করে তা পরিবর্তন করার পদক্ষেপ গ্রহন করবেন।
-        </p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If a customer
+                received a manufacturing defected product than the customer has to visit any of our shops where our
+                specialists will review the product first and then take the necessary steps to changing the product if
+                it is in need of replacement.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>ত্রুটিযুক্ত পণ্য
+                আমাদের স্টোর থেকে পরিবর্তন করা যাবে। এক্ষেত্রে আমাদের এক্সপার্টগন পণ্যে ত্রুটি পর্যবেক্ষণ করে তা
+                পরিবর্তন করার পদক্ষেপ গ্রহন করবেন।
+            </p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If a customer wants to change the defected product through our delivery service, then a charge Of TK. 200/- has to be paid as replacement charge inside Dhaka and if out of Dhaka, only the Courier charge is applicable.
-        </p>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>ক্রেতা যদি ডেলিভারি ম্যান এর মাধ্যমে ত্রুটিযুক্ত পণ্য পরিবর্তন করতে ইচ্ছুক থাকেন অথবা পণ্য পরিবর্তন করতে চান তবে ৩০০/- টাকা পরিবর্তন চার্জ প্রযোজ্য হবে। ঢাকার বাইরের ক্ষেত্রে শুধুমাত্র কুরিয়ার চার্জ প্রযোজ্য হবে। পণ্য আনার পর যদি পণ্য ভাঙ্গা অথবা পোড়া/ জ্বলা অবস্থায় পাওয়া যায় তবে সেই পণ্যের সম্পূর্ণ দায়িক্ত ক্রেতাকে বহন করতে হবে।
-        </p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If a customer wants
+                to change the defected product through our delivery service, then a charge Of TK. 200/- has to be paid
+                as replacement charge inside Dhaka and if out of Dhaka, only the Courier charge is applicable.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>ক্রেতা যদি ডেলিভারি
+                ম্যান এর মাধ্যমে ত্রুটিযুক্ত পণ্য পরিবর্তন করতে ইচ্ছুক থাকেন অথবা পণ্য পরিবর্তন করতে চান তবে ৩০০/- টাকা
+                পরিবর্তন চার্জ প্রযোজ্য হবে। ঢাকার বাইরের ক্ষেত্রে শুধুমাত্র কুরিয়ার চার্জ প্রযোজ্য হবে। পণ্য আনার পর
+                যদি পণ্য ভাঙ্গা অথবা পোড়া/ জ্বলা অবস্থায় পাওয়া যায় তবে সেই পণ্যের সম্পূর্ণ দায়িক্ত ক্রেতাকে বহন করতে
+                হবে।
+            </p>
+        </div>
 
-       <div class='my-[25px]'>
-       <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If a Customer is buying any product from our Website after reading the description and provided information’s about the particular product and is received successfully without any faults and later if the product is not compatible with your setup or if you do not want it anymore, then you will not be allowed to change or return the product.
-       </p>
-       <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>ল্যাপী ভ্যালি এর ওয়েবসাইটে থাকা বিবরণী দেখে ক্রয়ক্রিত পণ্য ডেলিভারি কর্মী থেকে রিসিভ করার পর তা আপনার নির্দিষ্ট ডিভাইসে সাপোর্ট করছে না অথবা তা এখন আর কিনতে ইচ্ছুক নন, এই জাতিয় কোন কারনে পণ্য ফেরত অথবা পরিবর্তন বা গ্রহনযোগ্য নয়।
-       </p>
-       
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If a Customer is
+                buying any product from our Website after reading the description and provided information’s about the
+                particular product and is received successfully without any faults and later if the product is not
+                compatible with your setup or if you do not want it anymore, then you will not be allowed to change or
+                return the product.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>ল্যাপী ভ্যালি এর
+                ওয়েবসাইটে থাকা বিবরণী দেখে ক্রয়ক্রিত পণ্য ডেলিভারি কর্মী থেকে রিসিভ করার পর তা আপনার নির্দিষ্ট ডিভাইসে
+                সাপোর্ট করছে না অথবা তা এখন আর কিনতে ইচ্ছুক নন, এই জাতিয় কোন কারনে পণ্য ফেরত অথবা পরিবর্তন বা গ্রহনযোগ্য
+                নয়।
+            </p>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If customers found broken or packet damaged product then we are requesting not to receive the product from courier service. If the customer receives courier damaged product then he/she have to take his/her own liability and any kind of complaint won’t be acceptable.
-        </p>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> কুরিয়ার এর ক্ষেত্রে ক্রেতা অবশ্যই পণ্য ভাঙ্গা থাকলে অথবা প্যাকেট ছেঁড়া থাকলে কুরিয়ার থেকে পণ্য রিসিভ করবে না। কুরিয়ারে ক্ষতিগ্রস্ত পণ্য ক্রেতা রিসিভ করলে তা নিজ দায়িত্বে করতে হবে এবং এই ব্যাপারে পরে কোন অভিযোগ গ্রহণযোগ্য হবে না।
-        </p>
-       </div>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>With specific reasoning’s, product(s) will be allowed to be returned and refunded back to you within 3 to 10 working days and in the case of online purchase, the procedure may take longer to execute.
-        </p>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>নির্দিষ্ট কারণে পণ্য রিটার্ন দেয়ার পর তার মূল্য রিফান্ড করতে ৩ থেকে ১০ কার্যদিবস বা অনলাইন পেমেন্টের ক্ষেত্রে আরও বেশি সময় লাগতে পারে।
-        </p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If customers found
+                broken or packet damaged product then we are requesting not to receive the product from courier service.
+                If the customer receives courier damaged product then he/she have to take his/her own liability and any
+                kind of complaint won’t be acceptable.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> কুরিয়ার এর ক্ষেত্রে
+                ক্রেতা অবশ্যই পণ্য ভাঙ্গা থাকলে অথবা প্যাকেট ছেঁড়া থাকলে কুরিয়ার থেকে পণ্য রিসিভ করবে না। কুরিয়ারে
+                ক্ষতিগ্রস্ত পণ্য ক্রেতা রিসিভ করলে তা নিজ দায়িত্বে করতে হবে এবং এই ব্যাপারে পরে কোন অভিযোগ গ্রহণযোগ্য
+                হবে না।
+            </p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>Refund charge applicable for Bkash / Online / POS payment refund.
-        </p>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>বিকাশ / অনলাইন / POS পেমেন্ট রিফান্ডের ক্ষেত্রে রিফান্ড চার্জ প্রযোজ্য ।
-        </p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>With specific
+                reasoning’s, product(s) will be allowed to be returned and refunded back to you within 3 to 10 working
+                days and in the case of online purchase, the procedure may take longer to execute.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>নির্দিষ্ট কারণে পণ্য
+                রিটার্ন দেয়ার পর তার মূল্য রিফান্ড করতে ৩ থেকে ১০ কার্যদিবস বা অনলাইন পেমেন্টের ক্ষেত্রে আরও বেশি সময়
+                লাগতে পারে।
+            </p>
+        </div>
 
-       <div class='my-[25px]'>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>To start a return, you can contact us at info@lappyvalley.com. If your return is accepted, we’ll send you a return shipping label, as well as instructions on how and where to send your package. 
-         Items sent back to us without first requesting a return will not be accepted.
-        </p>
-        <p class='text-[16px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>You can always contact us for any return question at info@lappyvalley.com
-        </p>
-       </div>
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>Refund charge
+                applicable for Bkash / Online / POS payment refund.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>বিকাশ / অনলাইন / POS
+                পেমেন্ট রিফান্ডের ক্ষেত্রে রিফান্ড চার্জ প্রযোজ্য ।
+            </p>
+        </div>
+
+        <div class='my-[0px]'>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>To start a return,
+                you can contact us at info@lappyvalley.com. If your return is accepted, we’ll send you a return shipping
+                label, as well as instructions on how and where to send your package.
+                Items sent back to us without first requesting a return will not be accepted.
+            </p>
+            <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>You can always
+                contact us for any return question at info@lappyvalley.com
+            </p>
+        </div>
     </div>
     <div class='h-[2px] bg-[#764A8733] mb-[150px]'></div>
 </div>
-
-
