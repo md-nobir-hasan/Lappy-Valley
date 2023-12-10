cbc99ab209649dafc8196d4c34f9397dbd8860f8
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-10 16:18:10 +0600

    wish list page responsive done

--- resources/views/livewire/wishlist.blade.php
+++ resources/views/livewire/wishlist.blade.php
@@ -1,71 +1,106 @@
-
-<div class='container mx-auto'>
-       <div class='flex justify-between'>
-       <h1 class='font-[jost] xl:text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Search / Lenovo</h1>
-       <div class='flex gap-[5px] items-center'><img src="/storage/product/vector-1.svg" alt="Product" class='w-[16px] h-[14px]'><span class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[23.12px]'>Log Out</span></div>
-       </div>
-        <div class='h-[2px] bg-[#764A8733]'></div>
-       <!-- --------------------user-name-------------------------------- -->
-       <div class='flex gap-[10px] items-center'>
-        <div class='w-[74px] h-[74px]'><img src="/storage/product/iconoir_profile-circle1.svg" alt="Product"></div>
-        <div>
-            <p class='text-[16px] text-[#000000] font-[Inter] font-[400] leadint-[14.52px]'>Hello,</p>
-            <h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leadint-[24.2px]'>Username Here</h1>
+<div class='px-[100px] max-md:px-[45px] max-sm:px-[20px] mx-auto max-sm:mt-[70px] max-xl:mt-[100px]'>
+    <div class='flex justify-between'>
+        <h1 class='font-[jost] xl:text-[20px] max-sm:text-[16px] max-md:text-[] max-lg:text-[] font-[400] leading-[25.3px] text-[#353535]'>Search / Lenovo</h1>
+        <div class='flex gap-[5px] items-center'><img src="/storage/product/vector-1.svg" alt="Product"
+                class='w-[16px] h-[14px]'><span
+                class='text-[16px] text-[#380D37] font-[jost] font-[400] leading-[23.12px]'>Log Out</span></div>
+    </div>
+    <div class='h-[2px] bg-[#764A8733]'></div>
+    <!-- --------------------user-name-------------------------------- -->
+    <div class='flex gap-[10px] items-center my-[20px]'>
+        <div class='w-[74px] h-[74px]'>
+            <img src="/storage/product/iconoir_profile-circle1.svg" alt="Product">
         </div>
-       </div>
+        <div>
+            <p class='text-[16px] max-sm:text-[14px] text-[#000000] font-[Inter] font-[400] leadint-[14.52px]'>
+                Hello,
+            </p>
+            <h1 class='text-[20px] max-sm:text-[16px] max-md:text-[] max-lg:text-[] text-[#000000] font-[Inter] font-[500] leadint-[24.2px]'>
+                Username Here
+            </h1>
+        </div>
+    </div>
 
-       <div class='flex gap-[100px] my-[30px]'>
+    <div
+        class='grid grid-cols-5 gap-[20px] max-sm:grid-cols-2 max-lg:grid-cols-3 max-lg:w-[500px] w-[1000px] my-[30px]'>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/icon-ad-product.svg"alt="Product" class='w-[23.36px] h-[26.21px]'>
-            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Orders</span>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>
+                Orders
+            </span>
         </div>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/iconoir_profile-circle.svg" alt="Product" class='w-[24px] h-[24px]'>
-            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Edit Profile</span>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>
+                Edit Profile
+            </span>
         </div>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/carbon_password.svg" alt="Product" class='w-[24px] h-[24px]'>
-            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Change Password</span>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>
+                Change Password
+            </span>
         </div>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/mdi_address-marker-outline.svg" alt="Product" class='w-[24px] h-[24px]'>
-            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Address</span>
+            <span class='text-[#AAAAAA] text-[16px] font-[jost] font-[500] leading-[23.12px]'>
+                Address
+            </span>
         </div>
         <div class='flex gap-[5px] items-center'>
             <img src="/storage/product/Vector-2.svg" alt="Product" class='w-[22.3px] h-[20.88px]'>
-            <span class='text-[#380D37] text-[16px] font-[jost] font-[500] leading-[23.12px]'>Wish List</span>
+            <span class='text-[#380D37] text-[16px] font-[jost] font-[500] leading-[23.12px]'>
+                Wish List
+            </span>
         </div>
-       </div>
-       <div class='h-[2px] bg-[#764A8733]'></div>
+    </div>
+    <div class='h-[2px] bg-[#764A8733]'></div>
 
-    
-        <div class='my-[15px]'>
-            <h1 class='text-[20px] text-[#000000] font-[Inter] font-[500] leading-24.2px[]'>Wish List:</h1>
-        </div>
-    <table class="container w-full">
+
+    <div class='my-[15px]'>
+        <h1 class='text-[20px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#000000] font-[Inter] font-[500] leading-24.2px[]'>
+            Wish List:
+        </h1>
+    </div>
+    <table class="w-full">
         <thead>
             <tr class="bg-[#380D37]">
-                <td class='py-3 px-6 text-[14px] text-[#FFFFFF] font-[jost] font-[500]'>Image</td>
-                <td class='py-3 px-6 text-[14px] text-[#FFFFFF] font-[jost] font-[500]'>Product Name</td>
-                <td class='py-3 px-6 text-[14px] text-[#FFFFFF] font-[jost] font-[500]'>Price</td>
-                <td class='text-[14px] text-[#FFFFFF] font-[jost] font-[500]'></td>
-                <td class='text-[14px] text-[#FFFFFF] font-[jost] font-[500]'></td>
-                </tr>
-            </thead>
-       
+                <td class='py-3 px-6 max-sm:py-[10px] max-sm:pr-[0px] max-sm:pl-[4px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>Image</td>
+                <td class='py-3 px-6 max-sm:py-[10px] max-sm:pr-[0px] max-sm:pl-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>Product Name</td>
+                <td class='py-3 px-6 max-sm:py-[10px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[] text-[14px] max-sm:text-[14px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'>Price</td>
+                <td class='text-[14px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'></td>
+                <td class='text-[14px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#FFFFFF] font-[jost] font-[500]'></td>
+            </tr>
+        </thead>
+
         <tbody class='items-center'>
-           <td class="py-3 px-6"><img src="/storage/product/large-size-laptop.jpg" alt="Product" class='w-[48px] h-[48px]'></td>
-           <td class="py-3 px-6"><span class='text-[20px] text-[#000000] font-[jost] font-[500] leading-[28.9px]'>Lenovo IdeaPad 15AMN7 AMD Ryzen 5 7520U</span></td>
-           <td class="py-3 px-6"><span class='text-[20px] text-[#DC275C] font-[jost] font-[700] leading-[28.9px]'>1,50,000 ৳</span></td>
-           <td class="py-3 px-6"><button class='text-[14px] text-[#F2F2F2] font-[jost] font-[500] rounded-[4px] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C] w-[133px] h-[41px]'>Buy Now</button></td>
-           <td class="py-3 px-2"><img src="/storage/product/cross-icon.svg" class="w-[28px] h-[28px] text-[#000000]" alt="product"></td>
+            <td class="py-3 px-6 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+                <img src="/storage/product/large-size-laptop.jpg" alt="Product"
+                    class='w-[48px] h-[48px]'>
+                </td>
+            <td class="py-3 px-6 max-sm:py-[2px] max-sm:pr-[0px] max-sm:pl-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+                <span
+                    class='text-[20px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#000000] font-[jost] font-[500] leading-[28.9px]'>
+                    Lenovo IdeaPad 15AMN7 AMD Ryzen 5 7520U
+                </span>
+            </td>
+            <td class="py-3 px-6 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+                <span
+                    class='text-[20px] max-sm:text-[12px] max-md:text-[] max-lg:text-[] text-[#DC275C] font-[jost] font-[700] leading-[28.9px]'>
+                    1,50,000 ৳
+                </span>
+            </td>
+            <td class="py-3 px-6 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+                <button
+                    class='text-[14px] max-sm:text-[10px] max-md:text-[] max-lg:text-[] text-[#F2F2F2] font-[jost] font-[500] rounded-[4px] leading-[20.23px] bg-gradient-to-r from-[#380D37] to-[#DC275C] w-[133px] max-sm:w-[70px] max-sm:h-[25px] h-[41px]'>
+                    Buy Now
+                </button>
+            </td>
+            <td class="py-3 px-2 max-sm:py-[2px] max-sm:px-[0px] max-md:py-[] max-md:px-[] max-lg:py-[] max-lg:px-[] max-xl:py-[] max-xl:px-[]">
+                <img src="/storage/product/cross-icon.svg" class="w-[28px] h-[28px] text-[#000000]" alt="product">
+            </td>
         </tbody>
     </table>
-       <div class='h-[1px] bg-[#380D37]'></div>
-       <div class="h-[2px] bg-[#764A8733] mt-[200px] "></div>
-    </div>
-
-          
-
-           
-    
+    <div class='h-[1px] bg-[#380D37]'></div>
+    <div class="h-[2px] bg-[#764A8733] mt-[200px] "></div>
+</div>
