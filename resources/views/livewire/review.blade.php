da002c82f6dd63475f33c03dd94cf580bcb7e845
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-13 00:16:06 +0600

    Update review.blade.php

--- resources/views/livewire/review.blade.php
+++ resources/views/livewire/review.blade.php
@@ -1,102 +1,80 @@
-<div class="px-[100px] mt-[150px]">
+<div class="px-[200px] mt-[100px]">
     <style>
-        /* .menu-toggle1 {
+           /* body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+        } */
+
+        /* Menu Icon and Close Icon Styles */
+        #menu-icon, #close-icon {
             cursor: pointer;
-            padding: 15px;
-            position:;
-            top: 5px;
-            right: 0px;
-            z-index: 2;
-            background: #fff
         }
 
-        .menu1 {
+        /* Menu Styles */
+        #menu {
             position: fixed;
-            top: 66px;
-            right: 300px;
-            width:300px;
-            height: 100vh;
-            background-color: white;
-            z-index: 1;
-            transition: .5s;
+            top: 0;
+            right: -300px; /* Start off-screen */
+            width: 300px;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
+            backdrop-filter: blur(8px); /* Apply blur effect */
+            z-index: 2;
+            transition: right 0.3s ease; /* Animation transition */
         }
-        .active.menu1 {
-            right: 0px !important;
-            color: #fff;
-        } */
+
+        /* Your Menu Content Styles */
+        #menu-content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            text-align: center;
+        }
     </style>
-    <div class="menu-toggle1 flex flex-col items-center" onclick="">
-        <div class="w-[25px] h-[2px] bg-[#380D37] my-[2px]"></div>
-        <div class="w-[18px] h-[2px] bg-[#380D37] my-[2px]"></div>
-        <div class="w-[10px] h-[2px] bg-[#380D37] my-[2px]"></div>
-    </div>
-    <div class="menu1 right-[-300px] w-[300px] h-[100vh] bg-[#f2f2f2]">
-        <div>
-            <h1 class="text-white">I am sumon</h1>
+</head>
+<body>
+
+    <!-- Menu Icon -->
+    <div id="menu-icon">☰</div>
+
+    <!-- Menu -->
+    <div id="menu">
+        <div id="menu-content">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <!-- Close Icon -->
+            <div id="close-icon">✕</div>
         </div>
     </div>
 
-   
-
+    {{-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script> --}}
     <script>
+        $(document).ready(function() {
+            var menu = $("#menu");
 
+            // Show menu when clicking on the menu icon
+            $("#menu-icon").click(function() {
+                menu.css("right", "0");
+            });
 
-      
-        //  $(document).ready(function() {
-        //             $('.menu-toggle1') {
-        //                 $(this).on('click', function() {
-        //                     // $(this).addClass('right-0 ');
-        //                     // $(this).removeClass('');
-        //                     $('.menu1').removeClass('right-[-300px]')
-        //                     $('.menu1').addClass('right-0 fixed top-[66px]');
-        //                     // $('.asian_prds').eq(index).hide();
-        //                     // $('.usa_prds').eq(index).show();
+            // Hide menu when clicking on the close icon
+            $("#close-icon").click(function() {
+                menu.css("right", "-300px");
+            });
 
-        //                 });
-        //             });
-
-        //
-        // $('.asian_btn').each(function(index) {
-        //     $(this).on('click', function() {
-        //         $(this).addClass('bg-[#380D37] text-[#F2F2F2]');
-        //         $(this).removeClass('bg-[#F2F2F2] text-[#380D37]');
-        //         $('.usa_btn').eq(index).removeClass('bg-[#380D37] text-[#F2F2F2]')
-        //         $('.usa_btn').eq(index).addClass('bg-[#F2F2F2] text-[#380D37]');
-        //         $('.usa_prds').eq(index).hide();
-        //         $('.asian_prds').eq(index).show();
-
-        //     });
-        // });
-        // document.addEventListener("DOMContentLoaded", function() {
-        //     var menuToggle = document.querySelector('.menu-toggle1');
-        //     var menu = document.querySelector('.menu1');
-
-        //     menuToggle.addEventListener('click', function(event) {
-        //         event.stopPropagation(); //
-        //         menuToggle.classList.toggle('active');
-        //         menu.classList.toggle('active');
-        //         toggleBodyOverflow(); // 
-        //     });
-
-        //     document.addEventListener('click', function(event) {
-        //         var isClickInsideMenu = menu.contains(event.target);
-        //         var isClickOnMenuToggle = menuToggle.contains(event.target);
-
-        //         if (!isClickInsideMenu && !isClickOnMenuToggle) {
-        //             menu.classList.remove('active');
-        //             menuToggle.classList.remove('active');
-        //             toggleBodyOverflow(); // Reset body overflow
-        //         }
-        //     });
-
-        //     function toggleBodyOverflow() {
-        //         // Check if menu is active and adjust body overflow
-        //         if (menu.classList.contains('active')) {
-        //             document.body.style.overflow = 'hidden';
-        //         } else {
-        //             document.body.style.overflow = '';
-        //         }
-        //     }
-        // });
+            // Hide menu when clicking outside the menu
+            $(document).mouseup(function(e) {
+                if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+                    menu.css("right", "-300px");
+                }
+            });
+        });
     </script>
+
 </div>
