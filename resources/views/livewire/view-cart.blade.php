bbf23d20a653975cec341a2894ce7eb386344563
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-12 10:33:08 +0600

    commit

--- resources/views/livewire/view-cart.blade.php
+++ resources/views/livewire/view-cart.blade.php
@@ -1,4 +1,4 @@
- <div class="container mx-auto">
+ <div class="px-[100px] max-md:px-[45px] max-lg:px-[70px] max-sm:px-[15px] mx-auto max-sm:mt-[70px] max-xl:mt-[100px]">
      <div class="max-xl:mt-[100px]">
          <h1 class='font-[jost] xl:text-[20px] font-[400] leading-[25.3px] text-[#353535]'>
             Shopping Cart/View Cart
@@ -10,56 +10,56 @@
          <div class="my-[10px]">
              <h1 class="text-[#353535] text-[20px] font-[jost] font-[500]">Shopping Cart</h1>
          </div>
-         <div class="relative overflow-x-auto">
-             <table class="w-full text-sm text-left text-[#FFFFFF] rtl:text-right text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500] bg-[#380D37]">
-                 <thead class="text-xs text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] text-[#FFFFFF]  bg-[#380D37] max-sm:h-[10px] h-[37px]">
+         <div class="w-full">
+             <table class="text-left w-full text-[#FFFFFF] rtl:text-right text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500] bg-[#380D37]">
+                 <thead class="w-full text-xs text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] text-[#FFFFFF]  bg-[#380D37] max-sm:h-[34px] h-[37px]">
                      <tr>
-                         <th scope="col" class='px-6 py-3 max-sm:px-[0px] max-md:px-[] max-lg:pl-[10px] max-lg:pr-[0px] max-xl:px-[] max-sm:py-[0px] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500]'>
+                         <th scope="col" class='px-4 py-3 max-sm:px-[0px] max-md:px-[] max-lg:pl-[10px] max-lg:pr-[0px] max-xl:px-[] max-sm:py-[0px] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500]'>
                          Image
                          </th>
-                         <th scope="col"  class='px-6 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[] max-sm:py-[0] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500]'>
+                         <th scope="col"  class='px-4 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[] max-sm:py-[0] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500]'>
                              Product name
                          </th>
-                         <th scope="col"  class='px-16 py-3 max-sm:pr-[6px] max-sm:pl-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[] max-sm:py-[0] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500]'>
+                         <th scope="col"  class='px-6 py-3 max-sm:pr-[0px]  max-md:px-[] max-lg:px-[0px] max-xl:px-[] max-sm:py-[0] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500]'>
                              Model
                          </th>
-                         <th scope="col"  class=' pl-6 pr-[12rem] max-sm:pr-[0px] max-sm:pl-[0px] max-sm:py-[0px] py-3 text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500]'>
+                         <th scope="col"  class=' pl-6 pr-[12rem] max-sm:pr-[0px] max-sm:pl-[0px] max-sm:py-[0px] py-3 text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500]'>
                              Quantity
                          </th>
-                         <th scope="col"  class='px-16 py-3 max-sm:pr-[0px] max-sm:pl-[6px] max-md:px-[] max-lg:pl-[0px] max-lg:pr-[10px] max-xl:px-[0] max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500]'>
+                         <th scope="col"  class='px-6 py-3 max-sm:pr-[0px]  max-md:px-[] max-lg:pl-[0px] max-lg:pr-[10px] max-xl:px-[0] max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500]'>
                              Unit Price
                          </th>
-                         <th scope="col"  class='px-2 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0] max-xl:px-[] max-sm:py-[0] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] font-[jost] font-[500]'>
+                         <th scope="col"  class='px-2 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0] max-xl:px-[] max-sm:py-[0] max-md:py-[0px] max-lg:py-[] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] font-[jost] font-[500]'>
                              total
                          </th>
                      </tr>
                  </thead>
-                 <tbody>
+                 <tbody class="w-full">
                      @forelse ($carts as $cart)
                          <tr class="items-center bg-white">
                              <td scope="row"
-                                 class="whitespace-nowrap px-6 py-3 max-md:px-[0px] max-md:py-[0px]">
+                                 class="whitespace-nowrap px-4 py-3 max-md:px-[0px] max-md:py-[0px]">
                                  <img class="w-[48px] h-[48px] max-sm:w-[25px] max-sm:h-[25px] max-md:w-[35px] max-md:h-[35px]" src="{{ $cart->product->photo }}" alt="{{ $cart->product->title }}">
                              </td>
-                             <td class="text-[#000000] px-6 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[0] max-sm:py-[] max-md:py-[0px] max-lg:py-[0] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[8px] max-lg:text-[10px]  font-[jost] font-[500]">
+                             <td class="text-[#000000] px-4 py-3 max-sm:w-[110px] max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[0] max-sm:py-[] max-md:py-[0px] max-lg:py-[0] max-xl:py-[] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-lg:text-[10px]  font-[jost] font-[500]">
                                  {{ $cart->product->title }}
 
                              </td>
-                             <td class="text-[#000000] max-xl:text-center px-16 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:pl-[4px] max-xl:pr-0 max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[0] text-[14px] max-xl:text-[12px] max-md:text-[8px] max-lg:text-[10px]  font-[jost] font-[500]">
+                             <td class="text-[#000000] max-xl:text-center max-sm:w-[50px] px-6 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:pl-[4px] max-xl:pr-0 max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[0] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-lg:text-[10px]  font-[jost] font-[500]">
                                  {{ $cart->product->model ?? '15AMN7' }}
                              </td>
-                             <td class="text-[#380D37] flex pr-[20rem] pl-[25px] py-3 max-sm:px-[0px] max-md:px-[] max-lg:pl-[25px] max-lg:pr-[] max-xl:pr-[5rem] max-sm:py-[5px] max-md:py-[0px] max-lg:py-[] max-xl:py-[]">
+                             <td class="text-[#380D37]">
                                 <div class="text-[#380D37] text-center flex gap-[10px] max-sm:gap-[5px] justify-center items-center">
-                                    <span class=" bg-[#F2F2F2] w-[78px] h-[39px] max-lg:w-[65px] max-md:w-[50px] max-sm:w-[22px]  max-lg:h-[32px] max-md:h-[25px] max-sm:h-[14px]  flex justify-center items-center text-[14px] max-xl:text-[12px] max-md:text-[8px] max-lg:text-[10px]  font-[jost] font-[500]">{{ $cart->quantity }} </span>
-                                    <span><img class="w-[20px] h-[20px] max-lg:w-[18px] max-md:w-[15px] max-sm:w-[10px] max-lg:h-[18px] max-md:h-[15px] max-sm:h-[10px]  text-[#000000]" src="/storage/product/swap.svg" alt="Product"></span>
-                                    <span><img class="w-[20px] h-[20px] max-lg:w-[18px] max-md:w-[15px] max-sm:w-[10px] max-lg:h-[18px] max-md:h-[15px] max-sm:h-[10px] text-[#000000]" src="/storage/product/cross-icon.svg" alt="Product"></span>
+                                    <span class=" bg-[#F2F2F2] font-[jost] font-[500]">{{ $cart->quantity }} </span>
+                                    <span><img class="w-[20px] h-[20px] text-[#000000]" src="/storage/product/swap.svg" alt="Product"></span>
+                                    <span><img class="w-[20px] h-[20px] text-[#000000]" src="/storage/product/cross-icon.svg" alt="Product"></span>
                                 </div>
                              </td>
 
-                             <td class="px-16 py-3 max-sm:px-[0px] max-md:px-[] max-lg:pl-[0px] max-lg:pr-[10px] max-xl:px-[0px] max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[0px] text-[#000000] text-[14px] max-xl:text-[12px] max-md:text-[8px] max-lg:text-[10px]  max-xl:text-center font-[jost] font-[500]">
+                             <td class="px-6 py-3 max-sm:w-[50px] max-sm:px-[0px] max-md:px-[] max-lg:pl-[0px] max-lg:pr-[10px] max-xl:px-[0px] max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[0px] text-[#000000] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-lg:text-[10px]  max-xl:text-center font-[jost] font-[500]">
                                 {{ number_format($cart->price) }}৳
                              </td>
-                             <td class="px-2 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[0px] max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[0px] text-[#000000] text-[14px] max-xl:text-[12px] max-md:text-[8px] max-lg:text-[10px] font-[jost] font-[500]">
+                             <td class="px-2 py-3 max-sm:px-[0px] max-md:px-[] max-lg:px-[0px] max-xl:px-[0px] max-sm:py-[] max-md:py-[0px] max-lg:py-[] max-xl:py-[0px] text-[#000000] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-lg:text-[10px] font-[jost] font-[500]">
 
                                 {{ $cart->amount }}৳
                              </td>
@@ -95,7 +95,7 @@
      {{-- <div class="flex justify-end my-[60px]">
          <a href="{{ route('checkout') }}" wire:navigate>
              <button
-                 class="bg-gradient-to-r from-[#380D37] to-[#DC275C] py-[10px] px-[40px] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[8px] text-[#fff] font-[jost] font-[500] rounded-[5px]">Confirm
+                 class="bg-gradient-to-r from-[#380D37] to-[#DC275C] py-[10px] px-[40px] text-[14px] max-xl:text-[12px] max-md:text-[10px] max-sm:text-[10px] text-[#fff] font-[jost] font-[500] rounded-[5px]">Confirm
                  Order</button>
          </a>
      </div>
