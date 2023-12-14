ff480dad3796059df57f6d207f21c3cd860f59ad
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-14 13:30:23 +0600

    receive-order ongoing

--- resources/views/livewire/order-received.blade.php
+++ resources/views/livewire/order-received.blade.php
@@ -1,3 +1,133 @@
-<div>
-    <h1 class="mt-12 text-center">Your order received successfully</h1>
+<div class='px-[100px] max-sm:px-[15px] max-lg:px-[45px] max-xl:px-[70px] max-sm:mt-[70px] max-xl:mt-[100px]'>
+    <div>
+        <h1 class='font-[jost] text-[16px] max-sm:text-[14px] font-[400] leading-[25.3px] text-[#353535]'>
+            Checkout / Online Payment
+        </h1>
+        <div class='h-[2px] bg-[#764A8733]'></div>
+    </div>
+    <div class="w-[650px] mx-auto max-xl:w-full bg-[#f2f2f2] p-6 mt-6 rounded-lg shadow">
+        <div>
+            <h1 class="text-center text-[22px] text-[#353535] font-[jost] font-[500]">
+                Your order has been placed!
+            </h1>
+            <p class="items-center text-center text-[14px] text-[#380D37] font-[jost] font-[400] py-[5px] px-[46px]">
+                Your order <span class="font-[jost] font-[700]">#398342</span> has been placed successfully. Should you have any questions about your order, feel free to call us on 09678002003 (10 AM - 5 PM).
+            </p>
+        </div>
+        <div class="bg-[#fff] rounded-lg shadow p-4 grid grid-cols-2">
+            <div class="flex flex-col">
+             <p class="text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px] mb-[10px]">
+                Delivery Address
+             </p>
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                ABDULLAH AL
+             </span>
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                Baridhara 33/4
+             </span>
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                Dhaka City
+             </span>
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+
+             </span>
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                Bangladesh
+             </span>
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                Mobile: 01309878889
+             </span>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px]">
+                            <th class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px]">Order Summary</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            Sub-Total
+                        </td>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            80,000৳
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            Home Delivery
+                        </td>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            60৳
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            Total
+                        </td>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            80,060৳
+                        </td>
+                      </tr>
+                      <tr>
+                       
+                      </tr>
+                      <tr>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            Amount Paid
+                        </td>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            80,060৳
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            Due
+                        </td>
+                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                            80,060৳
+                        </td>
+                      </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div>
+            <p>
+                Want to Pay Now? Please read carefully
+            </p>
+            <span>
+                After payment before agent confirmation, some products may not be delivered due to stock problems. To get refund, refund charge will be applicable and it will take 5 to 15 working days to process the refund. So we are encouraging you to pay after our agent's confirmation to avoid any inconveniences.
+            </span>
+        </div>
+        <div class='h-[2px] bg-[#764A8733]'></div>
+        <div>
+            <a href="#">Play Now</a>
+            <a href="#">Play Later</a>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <div>
+                <h1>
+                    Have a minute?
+                </h1>
+                <p>
+                    Like us on <span>Facebook</span> to keep you up to date with all our offers and announcements.
+                </p>
+            </div>
+            <div>
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class='h-[2px] bg-[#764A8733]'></div>
+        <div>
+            <span>
+                You may return product if you find any manufacturing flaw in it. You have 24 hours after delivery to return us the product. If it does not reach our office within 24 hours it will be considered as a warranty issue. The product should be returned with box and other included accessories. It has to be claimed by physically coming to the relative branch of Star Tech.
+            </span>
+        </div>
+
+        </div>
 </div>
