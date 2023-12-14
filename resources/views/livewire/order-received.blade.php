ded949bccb395475a69e9edbdc9b753580f24dcf
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-14 15:57:16 +0600

    online payment page done

--- resources/views/livewire/order-received.blade.php
+++ resources/views/livewire/order-received.blade.php
@@ -14,78 +14,78 @@
                 Your order <span class="font-[jost] font-[700]">#398342</span> has been placed successfully. Should you have any questions about your order, feel free to call us on 09678002003 (10 AM - 5 PM).
             </p>
         </div>
-        <div class="bg-[#fff] rounded-lg shadow p-4 grid grid-cols-2">
+        <div class="bg-[#fff] rounded-lg shadow p-4 grid grid-cols-2 max-sm:grid-cols-1">
             <div class="flex flex-col">
-             <p class="text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px] mb-[10px]">
+             <p class="text-[14px] text-[#380D37] font-[jost] font-[700] leading-[22px] mb-[10px]">
                 Delivery Address
              </p>
-             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[22px]">
                 ABDULLAH AL
              </span>
              <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
                 Baridhara 33/4
              </span>
-             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[22px]">
                 Dhaka City
              </span>
-             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[22px]">
 
              </span>
-             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[22px]">
                 Bangladesh
              </span>
-             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+             <span class="text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] my-[25px]">
                 Mobile: 01309878889
              </span>
             </div>
-            <div>
+            <div class="flex justify-end max-sm:justify-start">
                 <table>
                     <thead>
-                        <tr class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px]">
-                            <th class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px]">Order Summary</th>
+                        <tr class="text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px]">
+                            <th class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[700] leading-[20px]">Order Summary</th>
                         </tr>
                     </thead>
                     <tbody>
                       <tr>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             Sub-Total
                         </td>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap text-left">
                             80,000৳
                         </td>
                       </tr>
                       <tr>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             Home Delivery
                         </td>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap flex justify-end">
                             60৳
                         </td>
                       </tr>
                       <tr>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             Total
                         </td>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             80,060৳
                         </td>
                       </tr>
                       <tr>
                        
                       </tr>
-                      <tr>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                      <tr class="border-b-[1px] border-[#764A8733]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             Amount Paid
                         </td>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             80,060৳
                         </td>
                       </tr>
                       <tr>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             Due
                         </td>
-                        <td class="py-2 px-3 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px]">
+                        <td class="py-[5px] px-6 max-sm:pl-0 max-sm:pr-6 text-[14px] text-[#380D37] font-[jost] font-[400] leading-[20px] whitespace-nowrap">
                             80,060৳
                         </td>
                       </tr>
@@ -93,41 +93,54 @@
                 </table>
             </div>
         </div>
-        <div>
-            <p>
+        <div class="my-4 border-b-[2px] border-[#764A8733]">
+            <p class="text-[14px] text-[#380d37] font-[jost] font-[700] mb-3">
                 Want to Pay Now? Please read carefully
             </p>
-            <span>
+            <span class="text-[14px] text-[#380d37] font-[jost] font-[400]">
                 After payment before agent confirmation, some products may not be delivered due to stock problems. To get refund, refund charge will be applicable and it will take 5 to 15 working days to process the refund. So we are encouraging you to pay after our agent's confirmation to avoid any inconveniences.
             </span>
         </div>
-        <div class='h-[2px] bg-[#764A8733]'></div>
-        <div>
-            <a href="#">Play Now</a>
-            <a href="#">Play Later</a>
+        {{-- <div class='h-[2px] bg-[#764A8733]'></div> --}}
+        <div class="flex justify-end gap-4">
+            <a href="#" id="play-Now" class="px-[30px] py-[10px] border-[2px] border-[#380D37] text-[#380D37] text-[14px] font-[jost] font-[500] rounded-[4px]">Play Now</a>
+            <a href="#" id="play-Later" class="px-[30px] py-[10px] border-[2px] border-[#380D37] bg-gradient-to-r from-[#380D37] to-[#DC275C] text-[#f2f2f2] text-[14px] font-[jost] font-[500] rounded-[4px]">Play Later</a>
         </div>
     </div>
 
-    <div>
-        <div>
+    <div class="w-[650px] max-xl:w-full mx-auto p-3 my-[15px] bg-[#f2f2f2] rounded-lg shadow">
+        <div class="flex justify-between max-sm:justify-start max-sm:items-start max-sm:flex-col items-center mb-4 max-sm:gap-[20px]">
             <div>
-                <h1>
+                <h1 class="text-[14px] text-[#380d37] font-[jost] font-[700] mb-3">
                     Have a minute?
                 </h1>
-                <p>
+                <p class="text-[14px] text-[#380d37] font-[jost] font-[400]">
                     Like us on <span>Facebook</span> to keep you up to date with all our offers and announcements.
                 </p>
             </div>
-            <div>
-                <img src="" alt="">
+            <div class="w-[150px]">
+                <img src="/storage/product/find-us-on-facebook.png" alt="Product" class="w-[150px]">
             </div>
         </div>
         <div class='h-[2px] bg-[#764A8733]'></div>
-        <div>
-            <span>
+        <div class="my-4">
+            <span class="text-[14px] text-[#380d37] font-[jost] font-[400]">
                 You may return product if you find any manufacturing flaw in it. You have 24 hours after delivery to return us the product. If it does not reach our office within 24 hours it will be considered as a warranty issue. The product should be returned with box and other included accessories. It has to be claimed by physically coming to the relative branch of Star Tech.
             </span>
         </div>
 
         </div>
+        <script>
+        $(document).ready(function(){
+            $("#play-Now").click()
+            $(this).addClass("bg-gradient-to-r from-[#380D37] to-[#DC275C] text-[#f2f2f2]");
+            $("#play-Later").removeClass("bg-gradient-to-r from-[#380D37] to-[#DC275C] text-[#f2f2f2]");
+            $("#play-Later").addClass("text-[#380D37]");
+
+            $("#play-Later").click()
+            $(this).addClass("bg-gradient-to-r from-[#380D37] to-[#DC275C] text-[#f2f2f2]");
+            $("#play-Now").removeClass("bg-gradient-to-r from-[#380D37] to-[#DC275C] text-[#f2f2f2]");
+            $("#play-Now").addClass("text-[#380D37]");  
+        })
+        </script>
 </div>
