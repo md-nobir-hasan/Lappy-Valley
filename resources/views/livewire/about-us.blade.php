078aa9d62aa23cccd37d09ab1c9261e2c5379598
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-14 18:33:13 +0600

    about us responsive done

--- resources/views/livewire/about-us.blade.php
+++ resources/views/livewire/about-us.blade.php
@@ -1,42 +1,42 @@
-<div class="container mx-auto">
+<div class="px-[100px] max-sm:px-[15px] max-lg:px-[45px] max-xl:px-[60px] max-sm:mt-[70px] max-xl:mt-[100px]">
     <div class=''>
         <h1 class='font-[jost] text-[20px] font-[400] leading-[25.3px] text-[#353535]'>Home / About Us</h1>
     </div>
     <div class='h-[2px] bg-[#764A8733]'></div>
 
-    <div class="grid grid-cols-3 gap-4 items-center my-4">
-        <div class="items-center flex flex-col gap-4">
+    <div class="grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1  max-md:gap-4 max-xl:gap-0 gap-4 items-center my-4">
+        <div class="items-center flex flex-col max-lg:gap-2 gap-4">
             <div><img src="/storage/product/A-Rohman.png" alt=""></div>
 
             <div>
-                <h1 class="font-[jost] font-[700] text-[24px] text-[#353535]">Mohammad A Rahman</h1>
+                <h1 class="font-[jost] font-[700] text-[24px] max-md:text-[24px] max-lg:text-[16px] text-[#353535]">Mohammad A Rahman</h1>
             </div>
             <div>
-                <p class="font-[jost] font-[400] text-[18px] text-[#f78da7]">Entrepreneur</p>
+                <p class="font-[jost] font-[400] text-[18px] max-md:text-[18px] max-lg:text-[14px] text-[#f78da7]">Entrepreneur</p>
             </div>
 
         </div>
-        <div class="items-center flex flex-col gap-4">
+        <div class="items-center flex flex-col max-lg:gap-2 gap-4">
             <div><img src="/storage/product/Haque.jpg" alt=""></div>
             <div>
-                <h1 class="font-[jost] font-[700] text-[24px] text-[#353535]">Kazi Mazharul Haque</h1>
+                <h1 class="font-[jost] font-[700] text-[24px] max-md:text-[24px] max-lg:text-[16px] text-[#353535]">Kazi Mazharul Haque</h1>
             </div>
             <div>
-                <p class="font-[jost] font-[700] text-[18px] text-[#f78da7]">Entrepreneur</p>
+                <p class="font-[jost] font-[700] text-[18px] max-md:text-[18px] max-lg:text-[14px] text-[#f78da7]">Entrepreneur</p>
             </div>
         </div>
-        <div class="items-center flex flex-col gap-4">
+        <div class="items-center flex flex-col max-lg:gap-2 gap-4">
             <div><img src="/storage/product/mostafizur-rohman.jpeg" alt=""></div>
             <div>
-                <h1 class="font-[jost] font-[700] text-[24px] text-[#353535]">A.K.M. Mostafizur Rahman</h1>
+                <h1 class="font-[jost] font-[700] text-[24px] max-md:text-[24px]  max-lg:text-[16px] text-[#353535]">A.K.M. Mostafizur Rahman</h1>
             </div>
             <div>
-                <p class="font-[jost] font-[700] text-[18px] text-[#f78da7] items-center">Entrepreneur</p>
+                <p class="font-[jost] font-[700] text-[18px] max-md:text-[18px] max-lg:text-[14px] text-[#f78da7] items-center">Entrepreneur</p>
             </div>
         </div>
     </div>
 
-    <div class="my-[100px] flex flex-col text-left gap-8 w-[900px] mx-auto">
+    <div class="my-[100px] flex flex-col text-left gap-8 w-[900px] max-lg:w-full mx-auto">
         <div class="font-[jost] font-[700] text-[#353535] text-[24px]">
             <h1>About</h1>
         </div>
