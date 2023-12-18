67df545d4343d343d9424e8f5912f0e273bcfde3
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-18 11:27:55 +0600

    Update shop.blade.php

--- resources/views/livewire/shop.blade.php
+++ resources/views/livewire/shop.blade.php
@@ -1,4 +1,4 @@
-<div class="px-[100px] max-sm:px-[25px] max-md:px-[35px] max-lg:px-[45px] mx-auto max-sm:mt-[70px] max-xl:mt-[100px]">
+<div class="px-[100px] max-2xl:px-[70px] max-xl:px-[60px] max-lg:px-[38px] max-md:px-[35px] max-sm:px-[15px] max-sm:mt-[70px] max-xl:mt-[100px]">
     <style>
         ::selection {
             color: #fff;
