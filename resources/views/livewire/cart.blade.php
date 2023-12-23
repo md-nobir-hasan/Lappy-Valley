524c3194aec610e9921ed9eb22f06817c0bcf3b3
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-12-23 12:48:38 +0600

    Design feedback (#114)
    * commit
    
    * commit
    
    * commit
    
    * edit profile done
    
    * order-confirm page padding solution done
    
    * installment done
    
    * installment responsive done
    
    * Update product-deatils.blade.php
    
    * product-details page
    
    image up load when click mine image and show
    
    * Update product-deatils.blade.php
    
    * commit
    
    * Update output.css
    
    * add to cart and cross icon done
    
    ---------
    
    Co-authored-by: Sumon-Mia <sumon.wdd@gmail.com>

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -4,7 +4,7 @@
 <div id="side_cart" x-data="{ total: '{{ $carts?->sum('amount') }}' }"
     class="hidden w-[400px] max-sm:w-full max-sm:mt-[50px] bg-[#F2F2F2] right-0 top-0 fixed z-[9999] overflow-y-scroll">
     {{-- cart header (cart icon and close button) --}}
-    <div class="w-[400px] max-sm:w-full h-[61px] bg-[#380D37] flex justify-between text-[20px] text-[#fff] items-center px-[20px]">
+    <div class="w-[400px] max-sm:w-full h-[61px] bg-[#380D37] flex justify-between text-[20px] text-[#fff] items-center pl-[20px] pr-[30px]">
         <div class="flex items-center gap-[5px]">
             <img class='h-[24.84px] w-[25.6px]' src="/storage/product/cart(2).svg" alt="">
             <span class='font-[jost] cursor-pointer font-[500] text-[20px] text-[#F2F2F2]'>
@@ -12,7 +12,13 @@
             </span>
         </div>
         <div>
-            <h2 id="side_cart_close" class='font-[jost] cursor-pointer font-[500] text-[20px] text-[#F2F2F2]'>Close</h2>
+            {{-- <h2 id="side_cart_close" class='font-[jost] cursor-pointer font-[500] text-[20px] text-[#F2F2F2]'>Close</h2> --}}
+            <span id="side_cart_close" class="cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>
+            </span>
+              
         </div>
     </div>
 
