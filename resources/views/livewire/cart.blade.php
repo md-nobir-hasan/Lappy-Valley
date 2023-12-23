e62eb2bb5f6ed29c4569ae90ceef993d5b0d5562
Sumon-Mia <sumon.wdd@gmail.com>
2023-12-23 12:53:29 +0600

    see more button

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -18,7 +18,6 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                   </svg>
             </span>
-              
         </div>
     </div>
 
