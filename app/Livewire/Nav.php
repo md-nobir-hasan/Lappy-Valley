786c801b2a112bf64efc1d2c43a9520ee3b45766
Md. Nobir Hasan <nobir.wd@gmail.com>
2023-12-23 10:32:52 +0600

    Update Nav.php

--- app/Livewire/Nav.php
+++ app/Livewire/Nav.php
@@ -7,16 +7,18 @@ use Livewire\Component;
 
 class Nav extends Component
 {
-    public $menus;
     public $block;
     public function show($num){
         $this->block = $num;
     }
-    public function mount(){
-        $this->menus = Category::with('child_cat')->where('status', 'active')->where('is_parent', 1)->orderBy('serial', 'ASC')->get();
-    }
+
     public function render()
     {
-        return view('livewire.nav');
+        $n['menus'] = Category::with('child_cat')->where('status', 'active')->where('is_parent', 1)->orderBy('serial', 'ASC')->get();
+        if(count($n['menus'])>7){
+            $n['menus'] = $n['menus']->take(7);
+        }
+
+        return view('livewire.nav', $n);
     }
 }
