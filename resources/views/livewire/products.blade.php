52e4694259af7d065bba3ed52180ddecd2e8511b
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-01-29 10:56:47 +0600

    Report (#160)
    * report permission
    
    * report implementation
    
    * coupon currection
    
    * daily order report done
    
    * order report done
    
    * Update datewise.blade.php
    
    * Delete ~$quirments.docx
    
    * try to seed by using sql
    
    * preparing for sale report
    
    * preparing for sale report done
    
    * ad
    
    * Update datewise.blade.php
    
    * sale report done
    
    * Update ReportController.php
    
    * Update search.blade.php
    
    * Update AjaxController.php
    
    * Update user-manual.pptx
    
    * duration seeder curration
    
    * intallment ongoing
    
    * installment implementation done
    
    * space
    
    * mobile menu correction
    
    * search bar done
    
    * add to cart problem solve
    
    * Update NewProduct.php
    
    * after search show product er error currection
    
    * user order show
    
    * product update done
    
    * Update Checkout.php
    
    * checkout error done
    
    * home page
    
    * home page error done
    
    * Update 404.blade.php
    
    * discount price added

--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -54,7 +54,7 @@
                             value='{{ $product->final_price }}'>{{ number_format($product->final_price) }} ৳
                         </span>
                         <span class="text-[#380D37] text-[14px] font-[jost] font-[700] line-through">
-                            1200 ৳
+                            {{$product->price}} ৳
                         </span>
                     </div>
 
