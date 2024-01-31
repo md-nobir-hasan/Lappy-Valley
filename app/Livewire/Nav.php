430856ce014910609c808acfe08cdcf10f384ad4
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-01-31 10:53:07 +0600

    has product menus will appear

--- app/Livewire/Nav.php
+++ app/Livewire/Nav.php
@@ -8,17 +8,17 @@ use Livewire\Component;
 class Nav extends Component
 {
     public $block;
-    public function show($num){
+    public function show($num)
+    {
         $this->block = $num;
     }
 
     public function render()
     {
-        $n['menus'] = Category::with('child_cat')->where('status', 'active')->where('is_parent', 1)->orderBy('serial', 'ASC')->get();
-        if(count($n['menus'])>7){
+        $n['menus'] = Category::with(['child_cat', 'products', 'child_cat.sub_products'])->where('status', 'active')->where('is_parent', 1)->orderBy('serial', 'ASC')->get();
+        if (count($n['menus']) > 7) {
             $n['menus'] = $n['menus']->take(7);
         }
-
         return view('livewire.nav', $n);
     }
 }
