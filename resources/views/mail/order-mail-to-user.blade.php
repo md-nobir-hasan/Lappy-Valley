8731ab3c7425aae44f189a0475ec7be377e6d0d0
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-01-31 16:14:08 +0600

    Final checking (#164)
    * address correction
    
    * broadcamp currection done
    
    * order mail done
    
    * online payment paid
    
    * Update order-confirm.blade.php
    
    * delivered to received
    
    * Update order-mail-to-user.blade.php

--- resources/views/mail/order-mail-to-user.blade.php
+++ resources/views/mail/order-mail-to-user.blade.php
@@ -1,2 +1,171 @@
-<h1 style="text-align: center">Your order has been received</h1>
-<h1 style="text-align: center">Your order number {{$order->order_number}}</h1>
+<!-- Invoice styling -->
+
+<div>
+    <style>
+        body {
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            text-align: center;
+            color: #777;
+        }
+
+        body h1 {
+            font-weight: 300;
+            margin-bottom: 0px;
+            padding-bottom: 0px;
+            color: #000;
+        }
+
+        body h3 {
+            font-weight: 300;
+            margin-top: 10px;
+            margin-bottom: 20px;
+            font-style: italic;
+            color: #555;
+        }
+
+        body a {
+            color: #06f;
+        }
+
+        .invoice-box {
+            max-width: 800px;
+            margin: auto;
+            padding: 30px;
+            border: 1px solid #eee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+            font-size: 16px;
+            line-height: 24px;
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            color: #555;
+        }
+
+        .invoice-box table {
+            width: 100%;
+            line-height: inherit;
+            text-align: left;
+            border-collapse: collapse;
+        }
+
+        .invoice-box table td {
+            padding: 5px;
+            vertical-align: top;
+        }
+
+        .invoice-box table tr td:nth-child(2) {
+            text-align: right;
+        }
+
+        .invoice-box table tr.top table td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.top table td.title {
+            font-size: 45px;
+            line-height: 45px;
+            color: #333;
+        }
+
+        .invoice-box table tr.information table td {
+            padding-bottom: 40px;
+        }
+
+        .invoice-box table tr.heading td {
+            background: #eee;
+            border-bottom: 1px solid #ddd;
+            font-weight: bold;
+        }
+
+        .invoice-box table tr.details td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.item td {
+            border-bottom: 1px solid #eee;
+        }
+
+        .invoice-box table tr.item.last td {
+            border-bottom: none;
+        }
+
+        .invoice-box table tr.total td:nth-child(2) {
+            border-top: 2px solid #eee;
+            font-weight: bold;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .invoice-box table tr.top table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+
+            .invoice-box table tr.information table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+        }
+    </style>
+    <div class="invoice-box">
+        <table>
+            <tr class="top">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td class="title">
+                                <img src="{{asset($setting->logo)}}" alt="Company logo" style="width: 100%; max-width: 300px" />
+                            </td>
+
+                            <td>
+                                Invoice #: {{$order->order_number}}<br />
+                                Created: {{$order->created_at->format('d-M-Y')}}<br />
+                                {{-- Due: February 1, 2023 --}}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr class="information">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td>
+                               {{$order->address}}
+                            </td>
+
+                            <td>
+                               {{$order->email}}
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr class="heading">
+                <td>Payment Method</td>
+
+                <td>Check #</td>
+            </tr>
+
+            <tr class="details">
+                <td>{{$order->payment_method}}</td>
+                <td>{{$order->amount}}</td>
+            </tr>
+
+            <tr class="heading">
+                <td>Item</td>
+
+                <td>Price</td>
+            </tr>
+
+            @foreach ($order->cart_info as $cart)
+            <tr class="item">
+                <td>{{$cart->product->title}}</td>
+
+                <td>{{$cart->product->price}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
+</div>
