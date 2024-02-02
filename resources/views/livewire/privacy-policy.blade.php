399c7fd831e9d1cb52d462f3e33b3ce265e8c145
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-02-02 16:13:01 +0600

    home linking done

--- resources/views/livewire/privacy-policy.blade.php
+++ resources/views/livewire/privacy-policy.blade.php
@@ -1,6 +1,6 @@
 <div class='px-[100px] max-2xl:px-[70px] max-xl:px-[60px] max-lg:px-[38px] max-md:px-[35px] max-sm:px-[15px]'>
 <div class='max-sm:mt-[70px] max-xl:mt-[100px]'>
-            <h1 class='font-[jost] text-[16px] max-sm:text-[14px] font-[400] tracking-[.48px] text-[#353535]'>Home / Refund & Service Policy 
+            <h1 class='font-[jost] text-[16px] max-sm:text-[14px] font-[400] tracking-[.48px] text-[#353535]'><a href="{{route('home')}}" wire:navigate>Home</a> / Refund & Service Policy
         </div>
         <div class='h-[2px] bg-[#764A8733]'></div>
 
@@ -42,13 +42,13 @@
         <span  class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'>Cookies</span>
         <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment.
              These cookies will last for one year.</p>
-        <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If you visit our login page, we will set a temporary cookie to determine if your browser accepts cookies. 
+        <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>If you visit our login page, we will set a temporary cookie to determine if your browser accepts cookies.
             This cookie contains no personal data and is discarded when you close your browser.</p>
         <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> When you log in, we will also set up several cookies to save your login information and your screen display choices.
-            Login cookies last for two days, and screen options cookies last for a year. 
-            If you select “Remember Me”, your login will persist for two weeks. 
+            Login cookies last for two days, and screen options cookies last for a year.
+            If you select “Remember Me”, your login will persist for two weeks.
             If you log out of your account, the login cookies will be removed.</p>
-        <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If you edit or publish an article, an additional cookie will be saved in your browser. 
+        <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> If you edit or publish an article, an additional cookie will be saved in your browser.
             This cookie includes no personal data and simply indicates the post ID of the article you just edited.
             It expires after 1 day.</p>
        </div>
@@ -58,22 +58,22 @@
         <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'>Articles on this site may include embedded content (e.g. videos, images, articles, etc.).
            Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.</p>
         <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> These websites may collect data about you, use cookies, embed additional third-party tracking,
-            and monitor your interaction with that embedded content, 
+            and monitor your interaction with that embedded content,
             including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p>
         <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> Embedded content from other websites</p>
        </div>
 
        <div class='my-[10px]'>
         <span  class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[700] tracking-[.6px] italic'> Analytics </span>
-       
+
        </div>
 
        <div class='my-[10px]'>
         <h1 class='text-[20px] max-sm:text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px]'>Who we share your data with and How long we retain your data:</h1>
-        <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> Who we share your data with and How long we retain your data: If you leave a comment, the comment and its metadata are retained indefinitely. 
+        <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> Who we share your data with and How long we retain your data: If you leave a comment, the comment and its metadata are retained indefinitely.
             This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.</p>
         <p class='text-[16px] max-sm:text-[14px] text-[#353535] font-[jost] font-[400] tracking-[.48px] my-[10px]'> For users that register on our website (if any), we also store the personal information they provide in their user profile.
-             All users can see, edit, or delete their personal information at any time (except they cannot change their username). 
+             All users can see, edit, or delete their personal information at any time (except they cannot change their username).
             Website administrators can also see and edit that information.</p>
        </div>
 
@@ -89,7 +89,7 @@
        </div>
 
        <div class='my-[10px]'>
-        <h1 class='text-[20px] max-sm:text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px] 
+        <h1 class='text-[20px] max-sm:text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]
         transition duration-150 ease-in-out hover:text-[#ef4a23] decoration-[#ef4a23] decoration-2 hover:underline hover:underline-offset-4'>Your contact information</h1>
         <h1 class='text-[20px] max-sm:text-[20px] text-[#353535] font-[jost] font-[700] tracking-[.6px] block my-[10px]
         transition duration-150 ease-in-out hover:text-[#ef4a23] decoration-[#ef4a23] decoration-2 hover:underline hover:underline-offset-4'>Additional information</h1>
