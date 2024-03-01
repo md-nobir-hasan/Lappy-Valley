a2f1dc4b475695d20d05bc7a761bb75c30c0454d
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-03-01 12:20:37 +0600

    Update header.blade.php

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -2,22 +2,7 @@
     <div>
         <!-- Facebook Messenger Plugin Code -->
 <div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v12.0'
-    });
-  };
 
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-</script>
 
 <!-- Your Messenger Plugin Code -->
 <div class="fb-customerchat"
@@ -414,6 +399,24 @@
             });
         });
     </script>
+
+    {{-- messenger implementation code  --}}
+    <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v12.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
 @endscript
 
 @assets
