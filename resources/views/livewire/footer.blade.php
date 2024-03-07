b9f07ce0ba954ed92a4bff264b08ac2bee7c2957
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-03-07 15:33:16 +0600

    footer color

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -192,7 +192,7 @@
               </ul>
           </div>
       </div>
-      <div class="py-[5px] bg-[#7575cd] mt-[25px]">
+      <div class="py-[5px] bg-[#ff8181] mt-[25px]">
           <h3 class="text-center text-[14px] text-[#f2f2f2]">© {{ now()->format('Y') }} <a target="_blank"
                   href="{{ env('APP_URL') }}">lappyvalley.com</a>, All Rights Reserved || Developed By
               <a href="https://innovativeskillsbd.com/" target="_blank">Innovative Skills BD</a>
