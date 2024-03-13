3976516a1d75069489657a959c1fb388e768de02
Sumon-Mia <sumon.wdd@gmail.com>
2024-03-13 16:18:32 +0600

    table ongoing

--- resources/views/livewire/product-deatils.blade.php
+++ resources/views/livewire/product-deatils.blade.php
@@ -59,11 +59,11 @@
         </div>
         <div class="text-left max-md:mt-[40px]">
             <div>
-                <h1 class="font-[jost] text-[22px] font-[500] text-[#380D37] max-lg:text-[22px]">
+                <h1 class="font-[jost] text-[1.14rem] font-[500] text-[#380D37]">
                     {{ $product->title }}
                 </h1>
             </div>
-            <div class="flex text-center gap-[4px] mt-2">
+            <div class="flex text-center gap-[3px] mt-2">
                 <span>
                     <svg viewBox="0 0 24 24" fill="#72bf44" class="w-[20px] h-[20px]" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11.245 4.174C11.4765 3.50808 11.5922 3.17513 11.7634 3.08285C11.9115 3.00298 12.0898 3.00298 12.238 3.08285C12.4091 3.17513 12.5248 3.50808 12.7563 4.174L14.2866 8.57639C14.3525 8.76592 14.3854 8.86068 14.4448 8.93125C14.4972 8.99359 14.5641 9.04218 14.6396 9.07278C14.725 9.10743 14.8253 9.10947 15.0259 9.11356L19.6857 9.20852C20.3906 9.22288 20.743 9.23007 20.8837 9.36432C21.0054 9.48051 21.0605 9.65014 21.0303 9.81569C20.9955 10.007 20.7146 10.2199 20.1528 10.6459L16.4387 13.4616C16.2788 13.5829 16.1989 13.6435 16.1501 13.7217C16.107 13.7909 16.0815 13.8695 16.0757 13.9507C16.0692 14.0427 16.0982 14.1387 16.1563 14.3308L17.506 18.7919C17.7101 19.4667 17.8122 19.8041 17.728 19.9793C17.6551 20.131 17.5108 20.2358 17.344 20.2583C17.1513 20.2842 16.862 20.0829 16.2833 19.6802L12.4576 17.0181C12.2929 16.9035 12.2106 16.8462 12.1211 16.8239C12.042 16.8043 11.9593 16.8043 11.8803 16.8239C11.7908 16.8462 11.7084 16.9035 11.5437 17.0181L7.71805 19.6802C7.13937 20.0829 6.85003 20.2842 6.65733 20.2583C6.49056 20.2358 6.34626 20.131 6.27337 19.9793C6.18915 19.8041 6.29123 19.4667 6.49538 18.7919L7.84503 14.3308C7.90313 14.1387 7.93218 14.0427 7.92564 13.9507C7.91986 13.8695 7.89432 13.7909 7.85123 13.7217C7.80246 13.6435 7.72251 13.5829 7.56262 13.4616L3.84858 10.6459C3.28678 10.2199 3.00588 10.007 2.97101 9.81569C2.94082 9.65014 2.99594 9.48051 3.11767 9.36432C3.25831 9.23007 3.61074 9.22289 4.31559 9.20852L8.9754 9.11356C9.176 9.10947 9.27631 9.10743 9.36177 9.07278C9.43726 9.04218 9.50414 8.99359 9.55657 8.93125C9.61593 8.86068 9.64887 8.76592 9.71475 8.57639L11.245 4.174Z" stroke="#72bf44" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                 </span>
@@ -80,88 +80,117 @@
                     <svg viewBox="0 0 24 24" fill="#72bf44" class="w-[20px] h-[20px]" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11.245 4.174C11.4765 3.50808 11.5922 3.17513 11.7634 3.08285C11.9115 3.00298 12.0898 3.00298 12.238 3.08285C12.4091 3.17513 12.5248 3.50808 12.7563 4.174L14.2866 8.57639C14.3525 8.76592 14.3854 8.86068 14.4448 8.93125C14.4972 8.99359 14.5641 9.04218 14.6396 9.07278C14.725 9.10743 14.8253 9.10947 15.0259 9.11356L19.6857 9.20852C20.3906 9.22288 20.743 9.23007 20.8837 9.36432C21.0054 9.48051 21.0605 9.65014 21.0303 9.81569C20.9955 10.007 20.7146 10.2199 20.1528 10.6459L16.4387 13.4616C16.2788 13.5829 16.1989 13.6435 16.1501 13.7217C16.107 13.7909 16.0815 13.8695 16.0757 13.9507C16.0692 14.0427 16.0982 14.1387 16.1563 14.3308L17.506 18.7919C17.7101 19.4667 17.8122 19.8041 17.728 19.9793C17.6551 20.131 17.5108 20.2358 17.344 20.2583C17.1513 20.2842 16.862 20.0829 16.2833 19.6802L12.4576 17.0181C12.2929 16.9035 12.2106 16.8462 12.1211 16.8239C12.042 16.8043 11.9593 16.8043 11.8803 16.8239C11.7908 16.8462 11.7084 16.9035 11.5437 17.0181L7.71805 19.6802C7.13937 20.0829 6.85003 20.2842 6.65733 20.2583C6.49056 20.2358 6.34626 20.131 6.27337 19.9793C6.18915 19.8041 6.29123 19.4667 6.49538 18.7919L7.84503 14.3308C7.90313 14.1387 7.93218 14.0427 7.92564 13.9507C7.91986 13.8695 7.89432 13.7909 7.85123 13.7217C7.80246 13.6435 7.72251 13.5829 7.56262 13.4616L3.84858 10.6459C3.28678 10.2199 3.00588 10.007 2.97101 9.81569C2.94082 9.65014 2.99594 9.48051 3.11767 9.36432C3.25831 9.23007 3.61074 9.22289 4.31559 9.20852L8.9754 9.11356C9.176 9.10947 9.27631 9.10743 9.36177 9.07278C9.43726 9.04218 9.50414 8.99359 9.55657 8.93125C9.61593 8.86068 9.64887 8.76592 9.71475 8.57639L11.245 4.174Z" stroke="#72bf44" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                 </span>
                 <span>
-                    <a href="" class="text-[12px] text-[#000000] hover:text-[#919191] text-center decoration-none duration-200 font-[900]">
+                    <a href="" class="text-[12px] text-[#000000] hover:text-[#353535] text-center decoration-none duration-200 font-[500] font-[font-[jost]]">
                         10 reviews 
                     </a>
                 </span>
             </div>
-            <ul class="mb-3 mt-4 leading-[29px] max-lg:leading-[26px]">
-                <li class="font-[jost] text-[18px] font-[500] text-[#353535] max-lg:text-[18px]">Status:<span
-                        class="ml-2 text-[#DC275C]">
-                        @if ($product->upcomming)
-                            <span class="badge badge-success">UP COMMING (<span
-                                    class="badge badge-warning">{{ date('d-m-y', strtotime($product->upcomming)) }}</span>)
-                            </span>
-                            {{-- <span class="ml-5 badge badge-warning">{{$product->upcomming}}</span> --}}
-                        @else
-                            @if ($product->stock > 0)
-                                <span class="badge badge-success">IN STOCK</span>
-                            @else
-                                <span class="badge badge-warning">OUT OF STOCK</span>
-                            @endif
-                        @endif
-                    </span>
-                </li>
-                <li class="font-[jost] text-[18px] font-[500] text-[#353535] max-lg:text-[18px]">Key Features:
-                </li>
-                @if ($product->mpn)
-                    <li class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px]">
-                        Manufacture: {{ $product->mpn }}
-                    </li>
-                @endif
-                @if ($product->model)
-                    <li class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px]">
-                        Model: {{ $product->model }}
-                    </li>
-                @endif
-                @if ($product->ProcessorModel?->name)
-                    <li class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px]">
+            {{-- --------------- --}}
+            <!-- component -->
+<div class="flex flex-col">
+    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="overflow-hidden">
+          <table class="min-w-full text-center">
+            <thead class="border-b">
+              <tr>
+                <th scope="col" class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px] px-6 py-4">
+                   Attributes
+                </th>
+                <th scope="col" class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px] px-6 py-4">
+                 
+                </th>
+                <th scope="col" class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px] px-6 py-4">
+                    Value
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="border-b">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Default
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+              <tr class="border-b border-blue-200">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Primary
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+              <tr class="border-b bg-purple-100 border-purple-200">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Secondary
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+              <tr class="border-b bg-green-100">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Success
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+              <tr class="border-b bg-red-100 border-red-200">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Danger
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+              <tr class="border-b bg-yellow-100 border-yellow-200">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Warning
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
 
-                        Processor: {{ $product->ProcessorModel?->name }} @if ($product->c_speed)
-                            ({{ $product->c_speed }})
-                        @endif
-                    </li>
-                @endif
-                <li class="font-[jost] text-[16px] font-[400] text-[#353535] max-lg:text-[14px]">
-                    @if ($product->ram?->ram)
-                        RAM: {{ $product->ram?->ram }} GB,
-                    @endif
-
-                    @if ($product->storage())
-                        Storage: {{ $product->storage() }}
-                    @endif
-                </li>
-                <li class="font-[jost] text-[16px] font-[400] text-[rgb(53,53,53)] max-lg:text-[14px]">
-                    Display:
-                    {{ $product->DisplaySize?->size . '" ' . $product->DisplayType?->name . ' (' . $product->d_resolution }})
-                </li>
-                @if ($product->cat_info)
-                    <li class="font-[jost] text-[16px] font-[400] text-[rgb(53,53,53)] max-lg:text-[14px]">
-                        Condition:
-                        {{ $product->cat_info?->title }}
-                    </li>
-                @endif
-
-                <li class="mt-[10px] font-[jost] text-[18px] font-[500] text-[#353535] max-lg:text-[18px]">
-                    Regular Price:</li>
-                <li class="font-[jost] text-[18px] font-[700] text-[#DC275C] line-through max-lg:text-[18px]">
-                    <span>{{ $product->price }}</span> TK
-                </li>
-                @if ($product->discount)
-                    <li class="mt-[10px] font-[jost] text-[18px] font-[500] text-[#353535] max-lg:text-[18px]">
-                        Discount:
-                    </li>
-                    <li class="font-[jost] text-[18px] font-[700] text-[#DC275C] max-lg:text-[18px]">
-                        {{ $product->discount }}%
-                    </li>
-                @endif
-                <li class="mt-[10px] font-[jost] text-[18px] font-[500] text-[#353535] max-lg:text-[18px]">
-                    Discounted Price:
-                </li>
-                <li class="font-[jost] text-[18px] font-[700] text-[#DC275C] max-lg:text-[18px]">
-                    {{ $product->final_price }} TK
-                </li>
-            </ul>
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+              <tr class="border-b bg-indigo-100 border-indigo-200">
+                <td class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+                  Info
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                
+                </td>
+                <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  Cell
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+            {{-- --------------- --}}
+          
             <div>
                 <div>
                     <h1 class="font-[jost] text-[20px] font-[500] text-[#353535] max-lg:text-[18px]">
@@ -1196,7 +1225,7 @@
                                                 </div>
                                                 <div class="mt-[20px]">
                                                     <textarea x-model='msg' placeholder="Write your review here..."
-                                                        class="relative m-0 -mr-0.5 block w-full flex-auto rounded-l bg-[#f2f2f2] pb-[100px] pl-[10px] pt-[10px] font-[jost] font-[jost] text-[12px] font-[400] italic text-[#000000]"
+                                                        class="relative m-0 -mr-0.5 block w-full flex-auto rounded-l bg-[#f2f2f2] pb-[100px] pl-[10px] pt-[10px] font-[jost] text-[12px] font-[400] italic text-[#000000]"
                                                         id="message-text"></textarea>
                                                     @error('msg')
                                                         <span class="text-[12px] text-[red]">{{ $message }}</span>
