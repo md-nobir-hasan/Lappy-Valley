232858b519f585325a4521651e07f664bd0c73a7
Md. Nobir Hasan <nobir.wd@gmail.com>
2024-03-25 21:35:12 +0600

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    {{-- sentry video error  --}}
+    <script
+    src="https://js.sentry-cdn.com/93dc89a5646af7515c353ded6b155415.min.js"
+    crossorigin="anonymous"
+  ></script>
+  
     <!-- Open Graph meta tags for sharing -->
     <meta property="og:title" content="{{ ENV('APP_NAME') }}">
     <meta property="og:description"
